d20994fcf0e83afeb6931e917e428bc2f7e46f4d
Xiang Fan <sfanxiang@gmail.com>
2019-06-14 19:26:09 +0800

    pad_action_entry: Fix dangling pointer

--- src/pad_action_entry.rs
+++ src/pad_action_entry.rs
@@ -2,52 +2,52 @@
 // See the COPYRIGHT file at the top-level directory of this distribution.
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
-use glib::translate::*;
-use gtk_sys;
 use PadActionType;
 
-#[repr(C)]
-pub struct PadActionEntry(gtk_sys::GtkPadActionEntry);
+#[derive(Debug, Clone)]
+pub struct PadActionEntry {
+    type_: PadActionType,
+    index: i32,
+    mode: i32,
+    label: String,
+    action_name: String,
+}
 
 impl PadActionEntry {
-    pub fn new(type_: PadActionType, index: i32, mode: i32, label: &str,
-               action_name: &str) -> PadActionEntry {
+    pub fn new(
+        type_: PadActionType,
+        index: i32,
+        mode: i32,
+        label: &str,
+        action_name: &str,
+    ) -> PadActionEntry {
         assert_initialized_main_thread!();
-        PadActionEntry(gtk_sys::GtkPadActionEntry {
-            type_: type_.to_glib(),
+        PadActionEntry {
+            type_,
             index,
             mode,
-            label: label.to_glib_none().0,
-            action_name: action_name.to_glib_none().0,
-        })
+            label: label.to_owned(),
+            action_name: action_name.to_owned(),
+        }
     }
 
     pub fn get_type(&self) -> PadActionType {
-        from_glib(self.0.type_)
+        self.type_
     }
 
     pub fn get_index(&self) -> i32 {
-        self.0.index
+        self.index
     }
 
     pub fn get_mode(&self) -> i32 {
-        self.0.mode
+        self.mode
     }
 
-    pub fn get_label(&self) -> Option<String> {
-        unsafe { from_glib_none(self.0.label) }
+    pub fn get_label(&self) -> &str {
+        &self.label
     }
 
-    pub fn get_action_name(&self) -> Option<String> {
-        unsafe { from_glib_none(self.0.label) }
-    }
-}
-
-#[doc(hidden)]
-impl ToGlib for PadActionEntry {
-    type GlibType = gtk_sys::GtkPadActionEntry;
-
-    fn to_glib(&self) -> gtk_sys::GtkPadActionEntry {
-        self.0
+    pub fn get_action_name(&self) -> &str {
+        &self.action_name
     }
 }
--- src/pad_controller.rs
+++ src/pad_controller.rs
@@ -15,11 +15,32 @@ pub trait PadControllerExtManual: 'static {
 impl<O: IsA<PadController>> PadControllerExtManual for O {
     fn set_action_entries(&self, entries: &[PadActionEntry]) {
         let n_entries = entries.len() as i32;
-        let entries = entries.as_ptr();
+        let entry_strings = entries
+            .iter()
+            .map(|e| {
+                (
+                    e.get_label().to_glib_none(),
+                    e.get_action_name().to_glib_none(),
+                )
+            })
+            .collect::<Vec<_>>();
+        let entries = entries
+            .iter()
+            .zip(entry_strings.iter())
+            .map(|(e, (label, action_name))| gtk_sys::GtkPadActionEntry {
+                type_: e.get_type().to_glib(),
+                index: e.get_index(),
+                mode: e.get_mode(),
+                label: label.0,
+                action_name: action_name.0,
+            })
+            .collect::<Vec<_>>();
         unsafe {
-            gtk_sys::gtk_pad_controller_set_action_entries(self.as_ref().to_glib_none().0,
-                                                       mut_override(entries as *const _),
-                                                       n_entries);
+            gtk_sys::gtk_pad_controller_set_action_entries(
+                self.as_ref().to_glib_none().0,
+                entries.as_ptr(),
+                n_entries,
+            );
         }
     }
 }
