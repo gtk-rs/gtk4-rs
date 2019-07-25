ace29607c4c4a0f86fb19052653d602c1ead152b
Xiang Fan <sfanxiang@gmail.com>
2019-07-25 21:33:48 +0800

    build.rs: Remove event.rs from IGNORES

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
 fn manage_docs() {
     extern crate lgpl_docs;
     const PATH: &'static str = "src";
-    const IGNORES: &'static [&'static str] = &["event.rs", "lib.rs", "prelude.rs", "rt.rs"];
+    const IGNORES: &'static [&'static str] = &["lib.rs", "prelude.rs", "rt.rs"];
     lgpl_docs::purge(PATH, IGNORES);
     if cfg!(feature = "embed-lgpl-docs") {
         lgpl_docs::embed(lgpl_docs::Library::Gdk4, PATH, IGNORES);
