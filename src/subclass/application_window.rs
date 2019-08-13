9cb45c2df52e971531e1e9070d064ee1b259b621
Christopher Davis <brainblasted@disroot.org>
2019-08-13 13:57:01 -0700

    subclass: Add support for subclassing GtkApplicationWindow
    Like GtkBin this is an empty trait and `override_vfuncs`.

---
+++ src/subclass/application_window.rs
@@ -0,0 +1,15 @@
+use gtk_sys;
+
+use glib::translate::*;
+
+use glib::subclass::prelude::*;
+
+use super::window::WindowImpl;
+use ApplicationWindow;
+use ApplicationWindowClass;
+
+pub trait ApplicationWindowImpl: WindowImpl + 'static {}
+
+unsafe impl<T: ObjectSubclass + ApplicationWindowImpl> IsSubclassable<T> for ApplicationWindowClass {
+    fn override_vfuncs(&mut self) {}
+}
--- src/subclass/mod.rs
+++ src/subclass/mod.rs
@@ -3,19 +3,23 @@
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 pub mod application;
+pub mod application_window;
 pub mod bin;
 pub mod box_;
 pub mod container;
 pub mod event_box;
 pub mod widget;
+pub mod window;
 
 pub mod prelude {
     pub use super::application::GtkApplicationImpl;
+    pub use super::application_window::ApplicationWindowImpl;
     pub use super::bin::BinImpl;
     pub use super::box_::BoxImpl;
     pub use super::container::ContainerImpl;
     pub use super::event_box::EventBoxImpl;
     pub use super::widget::WidgetImpl;
+    pub use super::window::WindowImpl;
     pub use gio::subclass::prelude::*;
     pub use glib::subclass::prelude::*;
 }
