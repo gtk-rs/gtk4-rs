0297ff0211ef1c00e7479d77e110533b7e420537
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-10-19 15:54:12 +0200

    border node: remove manual peek_widths for now
    this needs proper fixing in the gir file first & regenerated sys crate

--- src/border_node.rs
+++ src/border_node.rs
@@ -3,11 +3,15 @@ use glib::translate::*;
 use glib::IsA;
 
 pub trait BorderNodeManualExt {
-    fn peek_widths(&self) -> f32;
+    fn peek_widths(&self) -> &[f32; 4];
 }
 
 impl<O: IsA<BorderNode>> BorderNodeManualExt for O {
-    fn peek_widths(&self) -> f32 {
-        unsafe { gsk_sys::gsk_border_node_peek_widths(self.as_ref().to_glib_none().0).read() }
+    fn peek_widths(&self) -> &[f32; 4] {
+        unsafe {
+            (gsk_sys::gsk_border_node_peek_widths(self.as_ref().to_glib_none().0)
+                as *const &[f32; 4])
+                .read()
+        }
     }
 }
