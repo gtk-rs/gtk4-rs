32d77126f9356e2d0081c8f5f9961a2c26bd6899
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2020-11-18 19:55:25 +0530

    Port builder_basics example from gtk-rs/examples4

---
+++ examples/src/bin/builder_basics.rs
@@ -0,0 +1,49 @@
+//! # Builder Basics Sample
+//!
+//! This sample demonstrates how to use the builder with an imported glade file
+
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use gtk::{ApplicationWindow, Builder, Button, MessageDialog, ResponseType};
+
+use std::env::args;
+
+fn build_ui(application: &gtk::Application) {
+    let glade_src = include_str!("builder_basics.glade");
+    let builder = Builder::from_string(glade_src);
+
+    let window: ApplicationWindow = builder.get_object("window1").expect("Couldn't get window1");
+    window.set_application(Some(application));
+    let bigbutton: Button = builder.get_object("button1").expect("Couldn't get button1");
+    let dialog: MessageDialog = builder
+        .get_object("messagedialog1")
+        .expect("Couldn't get messagedialog1");
+
+    dialog.connect_response(move |d: &MessageDialog, _: ResponseType| {
+        d.hide();
+    });
+
+    bigbutton.connect_clicked(move |_| {
+        dialog.show();
+    });
+
+    window.show();
+}
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.builder_basics"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+}
