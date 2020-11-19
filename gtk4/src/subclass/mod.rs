20a5f707cefa7b4557c20882f639269083dbd6ee
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-11-20 00:16:34 +0100

    gtk: add Sorter subclassing support

--- gtk4/src/subclass/mod.rs
+++ gtk4/src/subclass/mod.rs
@@ -16,6 +16,7 @@ pub mod layout_manager;
 pub mod list_box_row;
 pub mod native_dialog;
 pub mod popover;
+pub mod sorter;
 pub mod style_context;
 pub mod toggle_button;
 pub mod widget;
@@ -36,6 +37,7 @@ pub mod prelude {
     pub use super::list_box_row::ListBoxRowImpl;
     pub use super::native_dialog::NativeDialogImpl;
     pub use super::popover::PopoverImpl;
+    pub use super::sorter::SorterImpl;
     pub use super::style_context::StyleContextImpl;
     pub use super::toggle_button::ToggleButtonImpl;
     pub use super::widget::WidgetImpl;
