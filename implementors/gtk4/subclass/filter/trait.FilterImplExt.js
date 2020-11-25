87d6d63f53305a1c51782cd1e65c0c72def86e13
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-11-25 12:02:54 +0000

    Automated deployment: Wed Nov 25 12:02:54 UTC 2020 cc99604a5363ede76dd3ee40887e60af31958534

--- implementors/gtk4/subclass/filter/trait.FilterImplExt.js
+++ implementors/gtk4/subclass/filter/trait.FilterImplExt.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gtk4"] = [{"text":"impl&lt;T:&nbsp;ScrollableImpl&gt; IsImplementable&lt;T&gt; for Scrollable","synthetic":false,"types":[]}];
+implementors["gtk4"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
