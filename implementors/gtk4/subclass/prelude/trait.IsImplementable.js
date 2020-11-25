7a4d30f4b70012e16f6ba370a9553e325055fef7
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-11-25 13:29:17 +0000

    Automated deployment: Wed Nov 25 13:29:17 UTC 2020 4052c9518de851ee12ce0022a47890e6d2f2ac0e

--- implementors/gtk4/subclass/prelude/trait.IsImplementable.js
+++ implementors/gtk4/subclass/prelude/trait.IsImplementable.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gtk4"] = [{"text":"impl&lt;T:&nbsp;ScrollableImpl&gt; IsImplementable&lt;T&gt; for Scrollable","synthetic":false,"types":[]}];
+implementors["gtk4"] = [{"text":"impl&lt;T:&nbsp;ActionableImpl&gt; IsImplementable&lt;T&gt; for Actionable","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;EditableImpl&gt; IsImplementable&lt;T&gt; for Editable","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;ScrollableImpl&gt; IsImplementable&lt;T&gt; for Scrollable","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
