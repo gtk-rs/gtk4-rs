eb728b68ccdfb3415e7ce002956315f9f7455bd4
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-12-05 19:04:03 +0100

    gtk: implement From cmp::Ordering for Ordering (#101)

---
+++ gtk4/src/enums.rs
@@ -0,0 +1,25 @@
+use crate::Ordering;
+use std::cmp;
+
+impl From<cmp::Ordering> for Ordering {
+    fn from(o: cmp::Ordering) -> Self {
+        skip_assert_initialized!();
+        match o {
+            cmp::Ordering::Equal => Ordering::Equal,
+            cmp::Ordering::Greater => Ordering::Larger,
+            cmp::Ordering::Less => Ordering::Smaller,
+        }
+    }
+}
+
+impl From<Ordering> for cmp::Ordering {
+    fn from(o: Ordering) -> Self {
+        skip_assert_initialized!();
+        match o {
+            Ordering::Equal => cmp::Ordering::Equal,
+            Ordering::Larger => cmp::Ordering::Greater,
+            Ordering::Smaller => cmp::Ordering::Less,
+            Ordering::__Unknown(_) => unreachable!(),
+        }
+    }
+}
