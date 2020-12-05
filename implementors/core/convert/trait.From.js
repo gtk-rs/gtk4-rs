b12974acd431583e5bd13d330c18bb11a850bbf1
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-12-05 18:11:07 +0000

    Automated deployment: Sat Dec  5 18:11:07 UTC 2020 eb728b68ccdfb3415e7ce002956315f9f7455bd4

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["gtk4"] = [{"text":"impl From&lt;Ordering&gt; for Ordering","synthetic":false,"types":[]},{"text":"impl From&lt;Ordering&gt; for Ordering","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
