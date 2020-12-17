5f16edf0a3accbc66cd620847ac147cd046c5590
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-12-17 11:02:51 +0000

    Automated deployment: Thu Dec 17 11:02:51 UTC 2020 0eeb567e789b9455969d01f39448ae72e3a82080

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["gdk4"] = [{"text":"impl AsRef&lt;RectangleInt&gt; for Rectangle","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
