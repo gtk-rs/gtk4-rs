97d072b0f27f62539e2d817d32c2e70c4357b403
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-12-21 23:25:58 +0100

    gdk: add Paintable subclassing support
    part of #56

---
+++ examples/src/bin/custom_paintable.rs
@@ -0,0 +1,147 @@
+//! # Custom GdkPaintable
+//!
+//! This example shows how to create a custom gdk::Paintable
+//! that renders a square with a linear color gradient
+
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{gdk, glib, graphene, gsk};
+use std::env::args;
+
+mod imp {
+    use super::*;
+    use glib::subclass;
+    pub struct CustomPaintable {}
+
+    impl ObjectSubclass for CustomPaintable {
+        const NAME: &'static str = "CustomPaintable";
+        type Type = super::CustomPaintable;
+        type ParentType = glib::Object;
+        type Instance = subclass::simple::InstanceStruct<Self>;
+        type Class = subclass::simple::ClassStruct<Self>;
+
+        glib::object_subclass!();
+
+        fn type_init(type_: &mut subclass::InitializingType<Self>) {
+            type_.add_interface::<gdk::Paintable>();
+        }
+
+        fn new() -> Self {
+            Self {}
+        }
+    }
+
+    impl ObjectImpl for CustomPaintable {}
+
+    impl PaintableImpl for CustomPaintable {
+        fn get_flags(&self, _paintable: &Self::Type) -> gdk::PaintableFlags {
+            // Fixed size
+            gdk::PaintableFlags::SIZE
+        }
+
+        fn get_intrinsic_width(&self, _paintable: &Self::Type) -> i32 {
+            200
+        }
+
+        fn get_intrinsic_height(&self, _paintable: &Self::Type) -> i32 {
+            200
+        }
+
+        fn snapshot(
+            &self,
+            _paintable: &Self::Type,
+            snapshot: &gdk::Snapshot,
+            width: f64,
+            height: f64,
+        ) {
+            let snapshot = snapshot.downcast_ref::<gtk::Snapshot>().unwrap();
+            // Draw a linear gradient
+            snapshot.append_linear_gradient(
+                &graphene::Rect::new(0_f32, 0_f32, width as f32, height as f32),
+                &graphene::Point::new(0f32, 0f32),
+                &graphene::Point::new(width as f32, height as f32),
+                &[
+                    gsk::ColorStop::new(0.0, gdk::RGBA::red()),
+                    gsk::ColorStop::new(
+                        0.15,
+                        gdk::RGBA {
+                            red: 1.0,
+                            green: 127_f32 / 255_f32,
+                            blue: 0.0,
+                            alpha: 1.0,
+                        },
+                    ),
+                    gsk::ColorStop::new(
+                        0.3,
+                        gdk::RGBA {
+                            red: 1.0,
+                            green: 1.0,
+                            blue: 0.0,
+                            alpha: 1.0,
+                        },
+                    ),
+                    gsk::ColorStop::new(0.45, gdk::RGBA::green()),
+                    gsk::ColorStop::new(0.6, gdk::RGBA::blue()),
+                    gsk::ColorStop::new(
+                        0.75,
+                        gdk::RGBA {
+                            red: 75_f32 / 255_f32,
+                            green: 0.0,
+                            blue: 130_f32 / 255_f32,
+                            alpha: 1.0,
+                        },
+                    ),
+                    gsk::ColorStop::new(
+                        0.9,
+                        gdk::RGBA {
+                            red: 143_f32 / 255_f32,
+                            green: 0.0,
+                            blue: 1.0,
+                            alpha: 1.0,
+                        },
+                    ),
+                ],
+            );
+        }
+    }
+}
+
+glib::wrapper! {
+    pub struct CustomPaintable(ObjectSubclass<imp::CustomPaintable>) @implements gdk::Paintable;
+}
+
+impl CustomPaintable {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create a CustomPaintable")
+    }
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+    window.set_title(Some("Custom Paintable"));
+    window.set_default_size(500, 500);
+
+    let paintable = CustomPaintable::new();
+
+    let picture = gtk::Picture::new();
+    picture.set_halign(gtk::Align::Center);
+    picture.set_size_request(200, 200);
+    picture.set_paintable(Some(&paintable));
+
+    window.set_child(Some(&picture));
+    window.show();
+}
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.paintable"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+}
