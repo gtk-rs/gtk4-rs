878a38f73520a8ef2b1604d63d8d14b24a6b0301
Maximiliano Sandoval R <msandova@protonmail.com>
2021-01-01 18:20:13 +0100

    CustomSorter: Implement set_sort_func

---
+++ gtk4/src/custom_sorter.rs
@@ -0,0 +1,41 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+use crate::CustomSorter;
+use crate::Ordering;
+use glib::translate::*;
+
+impl CustomSorter {
+    #[doc(alias = "gtk_custom_sorter_set_sort_func")]
+    pub fn set_sort_func<F>(&self, sort_func: F)
+    where
+        F: Fn(&glib::Object, &glib::Object) -> Ordering + 'static,
+    {
+        unsafe {
+            ffi::gtk_custom_sorter_set_sort_func(
+                self.to_glib_none().0,
+                Some(trampoline::<F>),
+                Box::into_raw(Box::new(sort_func)) as *mut _,
+                Some(destroy_closure::<F>),
+            )
+        }
+    }
+}
+
+unsafe extern "C" fn destroy_closure<F: Fn(&glib::Object, &glib::Object) -> Ordering + 'static>(
+    ptr: glib::ffi::gpointer,
+) {
+    Box::<F>::from_raw(ptr as *mut _);
+}
+
+unsafe extern "C" fn trampoline<F: Fn(&glib::Object, &glib::Object) -> Ordering + 'static>(
+    a: glib::ffi::gconstpointer,
+    b: glib::ffi::gconstpointer,
+    f: glib::ffi::gpointer,
+) -> i32 {
+    let f: &F = &*(f as *const F);
+    f(
+        &from_glib_none(a as *mut glib::gobject_ffi::GObject),
+        &from_glib_none(b as *mut glib::gobject_ffi::GObject),
+    )
+    .to_glib()
+}
