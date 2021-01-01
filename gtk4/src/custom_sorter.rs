74d568443ec1cf5e0330f4310b10ed929fe1a89d
Maximiliano Sandoval R <msandova@protonmail.com>
2021-01-01 19:33:10 +0100

    CustomSorter: Implement new

--- gtk4/src/custom_sorter.rs
+++ gtk4/src/custom_sorter.rs
@@ -5,6 +5,21 @@ use crate::Ordering;
 use glib::translate::*;
 
 impl CustomSorter {
+    #[doc(alias = "gtk_custom_sorter_new")]
+    pub fn new<F>(sort_func: F) -> Self
+    where
+        F: Fn(&glib::Object, &glib::Object) -> Ordering + 'static,
+    {
+        assert_initialized_main_thread!();
+        unsafe {
+            from_glib_full(ffi::gtk_custom_sorter_new(
+                Some(trampoline::<F>),
+                Box::into_raw(Box::new(sort_func)) as *mut _,
+                Some(destroy_closure::<F>),
+            ))
+        }
+    }
+
     #[doc(alias = "gtk_custom_sorter_set_sort_func")]
     pub fn set_sort_func<F>(&self, sort_func: F)
     where
@@ -34,8 +49,8 @@ unsafe extern "C" fn trampoline<F: Fn(&glib::Object, &glib::Object) -> Ordering 
 ) -> i32 {
     let f: &F = &*(f as *const F);
     f(
-        &from_glib_none(a as *mut glib::gobject_ffi::GObject),
-        &from_glib_none(b as *mut glib::gobject_ffi::GObject),
+        &from_glib_borrow(a as *mut glib::gobject_ffi::GObject),
+        &from_glib_borrow(b as *mut glib::gobject_ffi::GObject),
     )
     .to_glib()
 }
