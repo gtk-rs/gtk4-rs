1150e81a8537c9c5bc4a9f14bd760464adc1cc43
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-01-09 20:03:04 +0000

    Automated deployment: Sat Jan  9 20:03:04 UTC 2021 397e7f9d6ea62b325e9e707dc76cd602a00e006e

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["gdk4"] = [{"text":"impl From&lt;RectangleInt&gt; for Rectangle","synthetic":false,"types":[]}];
-implementors["gtk4"] = [{"text":"impl From&lt;Ordering&gt; for Ordering","synthetic":false,"types":[]},{"text":"impl From&lt;Ordering&gt; for Ordering","synthetic":false,"types":[]}];
+implementors["gtk4"] = [{"text":"impl From&lt;Ordering&gt; for Ordering","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
