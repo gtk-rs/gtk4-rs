cd3e53b8bbcd2f1283b6248b22aa7679e172a6fa
sdroege <sdroege@users.noreply.github.com>
2021-01-13 07:40:51 +0000

    Automated deployment: Wed Jan 13 07:40:51 UTC 2021 13ea58bc76497fd2a613523c7c70ccc110523335

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["gdk4"] = [{"text":"impl Deref for Key","synthetic":false,"types":[]}];
-implementors["gtk4"] = [{"text":"impl Deref for Border","synthetic":false,"types":[]}];
+implementors["gtk4"] = [{"text":"impl&lt;T&gt; Deref for TemplateChild&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ObjectType + FromGlibPtrNone&lt;*mut &lt;T as ObjectType&gt;::GlibType&gt;,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl Deref for Border","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
