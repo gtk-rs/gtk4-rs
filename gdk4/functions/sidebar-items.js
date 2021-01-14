8350ed161ef56e3de912906b25b13cce47c3dddd
sdroege <sdroege@users.noreply.github.com>
2021-01-14 08:40:35 +0000

    Automated deployment: Thu Jan 14 08:40:35 UTC 2021 ba675562770e9713c0e97aab28760809ed50d93c

--- gdk4/functions/sidebar-items.js
+++ gdk4/functions/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["events_get_angle",""],["events_get_center",""],["events_get_distance",""],["intern_mime_type",""],["pixbuf_get_from_surface",""],["pixbuf_get_from_texture",""],["set_allowed_backends",""],["toplevel_size_get_type",""],["unicode_to_keyval",""]]});
+initSidebarItems({"fn":[["intern_mime_type",""],["pixbuf_get_from_surface",""],["pixbuf_get_from_texture",""],["set_allowed_backends",""],["toplevel_size_get_type",""],["unicode_to_keyval",""]]});
