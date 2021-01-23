9ba9ad1f32974b63c95ce2f1c3aafa68c0907a92
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-01-23 21:55:10 +0100

    gtk: fix get_*_size on ConstraintGuide

--- gtk4/src/constraint_guide.rs
+++ gtk4/src/constraint_guide.rs
@@ -3,46 +3,56 @@
 use crate::ConstraintGuide;
 use glib::translate::*;
 use glib::IsA;
+use std::mem::MaybeUninit;
 
 pub trait ConstraintGuideExtManual {
     #[doc(alias = "gtk_constraint_guide_get_max_size")]
-    fn get_max_size(&self, width: i32, height: i32);
+    fn get_max_size(&self) -> (i32, i32);
 
     #[doc(alias = "gtk_constraint_guide_get_min_size")]
-    fn get_min_size(&self, width: i32, height: i32);
+    fn get_min_size(&self) -> (i32, i32);
 
     #[doc(alias = "gtk_constraint_guide_get_nat_size")]
-    fn get_nat_size(&self, width: i32, height: i32);
+    fn get_nat_size(&self) -> (i32, i32);
 }
 
 impl<O: IsA<ConstraintGuide>> ConstraintGuideExtManual for O {
-    fn get_max_size(&self, width: i32, height: i32) {
+    fn get_max_size(&self) -> (i32, i32) {
         unsafe {
+            let mut width = MaybeUninit::uninit();
+            let mut height = MaybeUninit::uninit();
             ffi::gtk_constraint_guide_get_max_size(
                 self.as_ref().to_glib_none().0,
-                width as *mut _,
-                height as *mut _,
+                width.as_mut_ptr(),
+                height.as_mut_ptr(),
             );
+            (width.assume_init(), height.assume_init())
         }
     }
 
-    fn get_min_size(&self, width: i32, height: i32) {
+    fn get_min_size(&self) -> (i32, i32) {
         unsafe {
+            let mut width = MaybeUninit::uninit();
+            let mut height = MaybeUninit::uninit();
             ffi::gtk_constraint_guide_get_min_size(
                 self.as_ref().to_glib_none().0,
-                width as *mut _,
-                height as *mut _,
+                width.as_mut_ptr(),
+                height.as_mut_ptr(),
             );
+            (width.assume_init(), height.assume_init())
         }
     }
 
-    fn get_nat_size(&self, width: i32, height: i32) {
+    fn get_nat_size(&self) -> (i32, i32) {
         unsafe {
+            let mut width = MaybeUninit::uninit();
+            let mut height = MaybeUninit::uninit();
             ffi::gtk_constraint_guide_get_nat_size(
                 self.as_ref().to_glib_none().0,
-                width as *mut _,
-                height as *mut _,
+                width.as_mut_ptr(),
+                height.as_mut_ptr(),
             );
+            (width.assume_init(), height.assume_init())
         }
     }
 }
