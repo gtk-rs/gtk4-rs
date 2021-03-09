949fe147c832279a43628555b2f744ff90aa75b3
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-09 13:08:11 +0100

    gtk: flowbox & listbox are final
    no need to for ExtManual traits

--- gtk4/src/flow_box.rs
+++ gtk4/src/flow_box.rs
@@ -1,20 +1,15 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
 use crate::FlowBox;
-use glib::object::IsA;
 use glib::translate::*;
 use std::ptr;
 
-pub trait FlowBoxExtManual: 'static {
+impl FlowBox {
     #[doc(alias = "gtk_flow_box_bind_model")]
-    fn unbind_model(&self);
-}
-
-impl<O: IsA<FlowBox>> FlowBoxExtManual for O {
-    fn unbind_model(&self) {
+    pub fn unbind_model(&self) {
         unsafe {
             ffi::gtk_flow_box_bind_model(
-                self.as_ref().to_glib_none().0,
+                self.to_glib_none().0,
                 ptr::null_mut(),
                 None,
                 ptr::null_mut(),
