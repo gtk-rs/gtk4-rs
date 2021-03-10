80023c28fda08d0bdb226970cdf52291462a8f91
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-10 12:56:03 +0100

    gdk: manually implement ContentFormats::get_gtypes

---
+++ gdk4/src/content_formats.rs
@@ -0,0 +1,17 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+use crate::ContentFormats;
+use glib::translate::*;
+
+impl ContentFormats {
+    #[doc(alias = "gdk_content_formats_get_gtypes")]
+    pub fn get_gtypes(&self) -> Vec<glib::Type> {
+        unsafe {
+            let mut n_types = std::mem::MaybeUninit::uninit();
+            let types =
+                ffi::gdk_content_formats_get_gtypes(self.to_glib_none().0, n_types.as_mut_ptr());
+
+            FromGlibContainer::from_glib_container_num(types, n_types.assume_init() as usize)
+        }
+    }
+}
