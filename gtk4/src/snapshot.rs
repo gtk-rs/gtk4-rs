b8f15b511f3f9786fdef0689ace3f42c89e516a4
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-10 15:29:49 +0100

    gtk: manually implement Snapshot::append_color

--- gtk4/src/snapshot.rs
+++ gtk4/src/snapshot.rs
@@ -6,6 +6,25 @@ use graphene::{Point, Rect};
 use gsk::ColorStop;
 
 impl Snapshot {
+    #[doc(alias = "gtk_snapshot_append_border")]
+    pub fn append_border(
+        &self,
+        outline: &gsk::RoundedRect,
+        border_width: &[f32; 4],
+        border_color: &[gdk::RGBA; 4],
+    ) {
+        unsafe {
+            let border_color_ptr: Vec<gdk::ffi::GdkRGBA> =
+                border_color.iter().map(|c| *c.to_glib_none().0).collect();
+            ffi::gtk_snapshot_append_border(
+                self.to_glib_none().0,
+                outline.to_glib_none().0,
+                border_width,
+                border_color_ptr.as_ptr() as *const _,
+            )
+        }
+    }
+
     #[doc(alias = "gtk_snapshot_append_linear_gradient")]
     pub fn append_linear_gradient(
         &self,
