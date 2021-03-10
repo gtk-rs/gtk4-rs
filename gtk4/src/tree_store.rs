9854e1ea66b2a84518de29d624374150b9380f21
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-10 14:23:47 +0100

    gtk: manually implement TreeStore::set_column_types

--- gtk4/src/tree_store.rs
+++ gtk4/src/tree_store.rs
@@ -39,6 +39,9 @@ pub trait TreeStoreExtManual: 'static {
     #[doc(alias = "gtk_tree_store_set_valuesv")]
     fn set(&self, iter: &TreeIter, columns: &[u32], values: &[&dyn ToValue]);
 
+    #[doc(alias = "gtk_tree_store_set_column_types")]
+    fn set_column_types(&self, types: &[glib::Type]);
+
     #[doc(alias = "gtk_tree_store_set_value")]
     fn set_value(&self, iter: &TreeIter, column: u32, value: &Value);
 }
@@ -135,6 +138,17 @@ impl<O: IsA<TreeStore>> TreeStoreExtManual for O {
                 values.to_glib_none().0,
                 columns.len() as c_int,
             );
+        }
+    }
+
+    fn set_column_types(&self, types: &[glib::Type]) {
+        unsafe {
+            let types_ptr: Vec<glib::ffi::GType> = types.iter().map(|t| t.to_glib()).collect();
+            ffi::gtk_tree_store_set_column_types(
+                self.as_ref().to_glib_none().0,
+                types.len() as i32,
+                mut_override(types_ptr.as_ptr()),
+            )
         }
     }
 
