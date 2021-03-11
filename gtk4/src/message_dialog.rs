5d792d0da2b0d917b67bca5442216ade29cf0343
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-11 01:55:13 +0100

    gtk: manually implement MessageDialog markup methods
    From Rust you should rather call format! then pass the formatted string
    instead of using a weird C api.

--- gtk4/src/message_dialog.rs
+++ gtk4/src/message_dialog.rs
@@ -31,4 +31,45 @@ impl MessageDialog {
             .unsafe_cast()
         }
     }
+
+    #[doc(alias = "gtk_message_dialog_new_with_markup")]
+    pub fn with_markup<P: IsA<Window>>(
+        parent: Option<&P>,
+        flags: DialogFlags,
+        type_: MessageType,
+        buttons: ButtonsType,
+        message: Option<&str>,
+    ) -> MessageDialog {
+        assert_initialized_main_thread!();
+        unsafe {
+            Widget::from_glib_none(ffi::gtk_message_dialog_new_with_markup(
+                parent.map(|p| p.as_ref()).to_glib_none().0,
+                flags.to_glib(),
+                type_.to_glib(),
+                buttons.to_glib(),
+                message.to_glib_none().0,
+            ))
+            .unsafe_cast()
+        }
+    }
+
+    #[doc(alias = "gtk_message_dialog_format_secondary_markup")]
+    pub fn format_secondary_markup(&self, message: Option<&str>) {
+        unsafe {
+            ffi::gtk_message_dialog_format_secondary_markup(
+                self.to_glib_none().0,
+                message.to_glib_none().0,
+            )
+        }
+    }
+
+    #[doc(alias = "gtk_message_dialog_format_secondary_text")]
+    pub fn format_secondary_text(&self, message: Option<&str>) {
+        unsafe {
+            ffi::gtk_message_dialog_format_secondary_text(
+                self.to_glib_none().0,
+                message.to_glib_none().0,
+            )
+        }
+    }
 }
