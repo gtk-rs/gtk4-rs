6b5a26b2805090edabd3fd26fe2cd5fb017747de
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-11 11:33:29 +0100

    gtk: manually implement TreeModelFilter::set_modify_func

--- gtk4/src/tree_model_filter.rs
+++ gtk4/src/tree_model_filter.rs
@@ -2,8 +2,9 @@
 
 use glib::object::{Cast, IsA};
 use glib::translate::*;
+use std::boxed::Box as Box_;
 
-use crate::{TreeModel, TreeModelFilter, TreePath};
+use crate::{TreeIter, TreeModel, TreeModelFilter, TreePath};
 
 impl TreeModelFilter {
     #[doc(alias = "gtk_tree_model_filter_new")]
@@ -15,6 +16,60 @@ impl TreeModelFilter {
                 mut_override(root.to_glib_none().0),
             ))
             .unsafe_cast()
+        }
+    }
+}
+
+pub trait TreeModelFilterExtManual: 'static {
+    #[doc(alias = "gtk_tree_model_filter_set_modify_func")]
+    fn set_modify_func<F: Fn(&TreeModel, &TreeIter, i32) -> glib::Value + 'static>(
+        &self,
+        types: &[glib::Type],
+        func: F,
+    );
+}
+
+impl<O: IsA<TreeModelFilter>> TreeModelFilterExtManual for O {
+    #[doc(alias = "gtk_tree_model_filter_set_modify_func")]
+    fn set_modify_func<F: Fn(&TreeModel, &TreeIter, i32) -> glib::Value + 'static>(
+        &self,
+        types: &[glib::Type],
+        func: F,
+    ) {
+        unsafe {
+            let types_ptr: Vec<glib::ffi::GType> = types.iter().map(|t| t.to_glib()).collect();
+
+            unsafe extern "C" fn func_trampoline<
+                F: Fn(&TreeModel, &TreeIter, i32) -> glib::Value + 'static,
+            >(
+                model: *mut ffi::GtkTreeModel,
+                iter: *mut ffi::GtkTreeIter,
+                value: *mut glib::gobject_ffi::GValue,
+                column: i32,
+                user_data: glib::ffi::gpointer,
+            ) {
+                let f: &F = &*(user_data as *const F);
+                let ret = f(&from_glib_borrow(model), &from_glib_borrow(iter), column);
+                *value = ret.into_raw();
+            }
+
+            unsafe extern "C" fn destroy_func<
+                F: Fn(&TreeModel, &TreeIter, i32) -> glib::Value + 'static,
+            >(
+                user_data: glib::ffi::gpointer,
+            ) {
+                let _callback: Box_<Option<Box_<F>>> = Box_::from_raw(user_data as *mut _);
+            }
+            let callback_data: Box_<F> = Box_::new(func);
+
+            ffi::gtk_tree_model_filter_set_modify_func(
+                self.as_ref().to_glib_none().0,
+                types.len() as i32,
+                mut_override(types_ptr.as_ptr()),
+                Some(func_trampoline::<F> as _),
+                Box_::into_raw(callback_data) as *mut _,
+                Some(destroy_func::<F> as _),
+            )
         }
     }
 }
