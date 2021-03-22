311af7941f5222a5e8bb6fea8d4a4bb11fe9dabc
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-22 10:34:21 +0100

    gtk: add a check on StringFilter::[new|set_expression]
    StringFilter's expression must be either NULL or evaluate to a string

--- gtk4/src/string_filter.rs
+++ gtk4/src/string_filter.rs
@@ -7,6 +7,11 @@ impl StringFilter {
     #[doc(alias = "gtk_string_filter_new")]
     pub fn new<E: AsRef<Expression>>(expression: Option<&E>) -> StringFilter {
         assert_initialized_main_thread!();
+        if let Some(e) = expression {
+            if !e.as_ref().get_value_type().is_a(glib::Type::STRING) {
+                panic!("StringFilter::new must take either None or an expression that evaluates to a string.");
+            }
+        }
         unsafe {
             from_glib_full(ffi::gtk_string_filter_new(
                 expression.map(|e| e.as_ref()).to_glib_full(),
@@ -16,6 +21,11 @@ impl StringFilter {
 
     #[doc(alias = "gtk_string_filter_set_expression")]
     pub fn set_expression<E: AsRef<Expression>>(&self, expression: Option<&E>) {
+        if let Some(e) = expression {
+            if !e.as_ref().get_value_type().is_a(glib::Type::STRING) {
+                panic!("StringFilter::set_expression must take either None or an expression that evaluates to a string.");
+            }
+        }
         unsafe {
             ffi::gtk_string_filter_set_expression(
                 self.to_glib_none().0,
