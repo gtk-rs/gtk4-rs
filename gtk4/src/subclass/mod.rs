0cfdbf2ea72638fabce47d35e37d2535630f67f1
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-03-22 21:15:12 +0100

    gtk: add Grid subclassing support

--- gtk4/src/subclass/mod.rs
+++ gtk4/src/subclass/mod.rs
@@ -24,6 +24,7 @@ pub mod fixed;
 pub mod flow_box_child;
 pub mod frame;
 pub mod gl_area;
+pub mod grid;
 pub mod im_context;
 pub mod layout_manager;
 pub mod list_box_row;
@@ -87,6 +88,7 @@ pub mod prelude {
     pub use super::flow_box_child::{FlowBoxChildImpl, FlowBoxChildImplExt};
     pub use super::frame::{FrameImpl, FrameImplExt};
     pub use super::gl_area::{GLAreaImpl, GLAreaImplExt};
+    pub use super::grid::GridImpl;
     pub use super::im_context::{IMContextImpl, IMContextImplExt};
     pub use super::layout_manager::{LayoutManagerImpl, LayoutManagerImplExt};
     pub use super::list_box_row::{ListBoxRowImpl, ListBoxRowImplExt};
