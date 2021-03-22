4422bc0bcf8c9bbd6d2343016fa87275e816c7c5
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-03-22 10:20:55 +0000

    Automated deployment: Mon Mar 22 10:20:55 UTC 2021 311af7941f5222a5e8bb6fea8d4a4bb11fe9dabc

--- implementors/gdk4_wayland/trait.WaylandSurfaceManualExt.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["gdk4_wayland"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/gtk4/prelude/trait.MediaStreamExtManual.js
+++ implementors/gtk4/prelude/trait.MediaStreamExtManual.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gdk4_wayland"] = [];
+implementors["gtk4"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
