gtk-rs/gtk4-rs

commit c62171ab5fd7e1cdf97efb0c2c4337c4ce3ff0f9

Don't set a default value for the -d option

generator.py
@@ -79,7 +79,9 @@ def regen_crates(path, conf):
     elif path.match("Gir*.toml"):
         print('==> Regenerating "{}"...'.format(path))
 
-        args = [conf.gir_path, "-c", path, "-o", path.parent, "-d", conf.gir_files_path]
+        args = [conf.gir_path, "-c", path, "-o", path.parent]
+        if len(conf.gir_files_paths) > 0:
+            args += ["-d", conf.gir_files_paths]
         if path.parent.name.endswith("sys"):
             args.extend(["-m", "sys"])
         error = False
@@ -134,8 +136,9 @@ def parse_args():
     )
     parser.add_argument(
         "--gir-files-directory",
-        dest="gir_files_path",
-        default=DEFAULT_GIR_FILES_DIRECTORY,
+        nargs="+", # If the option is used, we expect at least one folder!
+        dest="gir_files_paths",
+        default=[],
         type=directory_path,
         help="Path of the gir-files folder",
     )
@@ -162,8 +165,8 @@ def parse_args():
 def main():
     conf = parse_args()
 
-    if conf.gir_files_path == DEFAULT_GIR_FILES_DIRECTORY:
-        if def_check_submodule(conf.gir_files_path, conf) == FAILURE:
+    if len(conf.gir_files_paths) == 0:
+        if def_check_submodule(DEFAULT_GIR_FILES_DIRECTORY, conf) == FAILURE:
             return 1
 
     if conf.gir_path == DEFAULT_GIR_PATH:

commit d62609fdbab1664d4aa5bd483c91f216edc58125

generator: Iterate gir directories passed on the command line
This change cleans up handling of the default gir directory, and makes
sure the `-d` flag is prepended to every path. Before the command would
contain a single `-d` flag followed by a `list` of paths, which cannot
be passed to an executable verbatim:

    The following error occurred: expected str, bytes or os.PathLike object, not list
    Do you want to continue? [y/N] ^CTraceback (most recent call last):
      File "./generator.py", line 189, in <module>

Fixes: c62171ab5 ("Don't set a default value for the -d option")

generator.py
@@ -79,9 +79,9 @@ def regen_crates(path, conf):
     elif path.match("Gir*.toml"):
         print('==> Regenerating "{}"...'.format(path))
 
-        args = [conf.gir_path, "-c", path, "-o", path.parent]
-        if len(conf.gir_files_paths) > 0:
-            args += ["-d", conf.gir_files_paths]
+        args = [conf.gir_path, "-c", path, "-o", path.parent] + [
+            d for path in conf.gir_files_paths for d in ("-d", path)
+        ]
         if path.parent.name.endswith("sys"):
             args.extend(["-m", "sys"])
         error = False
@@ -135,8 +135,8 @@ def parse_args():
         help="Paths in which to look for Gir.toml files",
     )
     parser.add_argument(
-        "--gir-files-directory",
-        nargs="+", # If the option is used, we expect at least one folder!
+        "--gir-files-directories",
+        nargs="+",  # If the option is used, we expect at least one folder!
         dest="gir_files_paths",
         default=[],
         type=directory_path,
@@ -165,7 +165,7 @@ def parse_args():
 def main():
     conf = parse_args()
 
-    if len(conf.gir_files_paths) == 0:
+    if not conf.gir_files_paths:
         if def_check_submodule(DEFAULT_GIR_FILES_DIRECTORY, conf) == FAILURE:
             return 1
 