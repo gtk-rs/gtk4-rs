33deff0f4a929bf994c6e9cdbb76059cccd57105
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-15 16:28:06 +0200

    gtk: CallbackAction::new's callback param shouldn't be nullable

--- gtk4/src/auto/callback_action.rs
+++ gtk4/src/auto/callback_action.rs
@@ -18,43 +18,34 @@ glib::wrapper! {
 
 impl CallbackAction {
     #[doc(alias = "gtk_callback_action_new")]
-    pub fn new(
-        callback: Option<Box_<dyn Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>>,
+    pub fn new<P: Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>(
+        callback: P,
     ) -> CallbackAction {
         assert_initialized_main_thread!();
-        let callback_data: Box_<
-            Option<Box_<dyn Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>>,
-        > = Box_::new(callback);
-        unsafe extern "C" fn callback_func(
+        let callback_data: Box_<P> = Box_::new(callback);
+        unsafe extern "C" fn callback_func<
+            P: Fn(&Widget, Option<&glib::Variant>) -> bool + 'static,
+        >(
             widget: *mut ffi::GtkWidget,
             args: *mut glib::ffi::GVariant,
             user_data: glib::ffi::gpointer,
         ) -> glib::ffi::gboolean {
             let widget = from_glib_borrow(widget);
             let args: Borrowed<Option<glib::Variant>> = from_glib_borrow(args);
-            let callback: &Option<Box_<dyn Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>> =
-                &*(user_data as *mut _);
-            let res = if let Some(ref callback) = *callback {
-                callback(&widget, args.as_ref().as_ref())
-            } else {
-                panic!("cannot get closure...")
-            };
+            let callback: &P = &*(user_data as *mut _);
+            let res = (*callback)(&widget, args.as_ref().as_ref());
             res.to_glib()
         }
-        let callback = if callback_data.is_some() {
-            Some(callback_func as _)
-        } else {
-            None
-        };
-        unsafe extern "C" fn destroy_func(data: glib::ffi::gpointer) {
-            let _callback: Box_<
-                Option<Box_<dyn Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>>,
-            > = Box_::from_raw(data as *mut _);
+        let callback = Some(callback_func::<P> as _);
+        unsafe extern "C" fn destroy_func<
+            P: Fn(&Widget, Option<&glib::Variant>) -> bool + 'static,
+        >(
+            data: glib::ffi::gpointer,
+        ) {
+            let _callback: Box_<P> = Box_::from_raw(data as *mut _);
         }
-        let destroy_call2 = Some(destroy_func as _);
-        let super_callback0: Box_<
-            Option<Box_<dyn Fn(&Widget, Option<&glib::Variant>) -> bool + 'static>>,
-        > = callback_data;
+        let destroy_call2 = Some(destroy_func::<P> as _);
+        let super_callback0: Box_<P> = callback_data;
         unsafe {
             from_glib_full(ffi::gtk_callback_action_new(
                 callback,
