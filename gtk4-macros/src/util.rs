f52fbb5abe6269d48f1abef987271b6778c1ab8f
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-19 15:18:15 +0200

    bump proc-macro-crate

--- gtk4-macros/src/util.rs
+++ gtk4-macros/src/util.rs
@@ -4,9 +4,11 @@ use proc_macro2::{Ident, Span};
 use proc_macro_crate::crate_name;
 
 pub fn crate_ident_new() -> Ident {
-    let crate_name = match crate_name("gtk4") {
-        Ok(x) => x,
-        Err(_) => "gtk4".to_owned(),
+    use proc_macro_crate::FoundCrate;
+
+    let crate_name = match crate_name("gtk4").expect("missing gtk4 dependency in `Cargo.toml`") {
+        FoundCrate::Name(name) => name,
+        FoundCrate::Itself => "gtk4".to_owned(),
     };
 
     Ident::new(&crate_name, Span::call_site())
