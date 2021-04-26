8751c42fa9e024ed602373aa8fb499834313ef33
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-26 16:42:01 +0200

    gtk: IconTheme is final

--- gtk4/src/icon_theme.rs
+++ gtk4/src/icon_theme.rs
@@ -3,18 +3,11 @@
 use crate::IconTheme;
 use glib::translate::*;
 
-pub trait IconThemeExtManual {
+impl IconTheme {
     #[doc(alias = "gtk_icon_theme_set_resource_path")]
-    fn set_resource_path(&self, path: &str);
-}
-
-impl<O: AsRef<IconTheme>> IconThemeExtManual for O {
-    fn set_resource_path(&self, path: &str) {
+    pub fn set_resource_path(&self, path: &str) {
         unsafe {
-            ffi::gtk_icon_theme_set_resource_path(
-                self.as_ref().to_glib_none().0,
-                path.as_ptr() as *const _,
-            );
+            ffi::gtk_icon_theme_set_resource_path(self.to_glib_none().0, path.as_ptr() as *const _);
         }
     }
 }
