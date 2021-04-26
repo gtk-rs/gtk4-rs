b26954a0bb0e9ff36deed5a17be24ab23da5d503
Julian Hofer <julianhofer@gnome.org>
2021-04-26 13:28:51 +0200

    Explain how to use boxed types with `Value`

--- book/listings/gobject_values/1/main.rs
+++ book/listings/gobject_values/1/main.rs
@@ -1,12 +1,36 @@
 use gtk::prelude::*;
 
 fn main() {
-    // Convert `i32` to `Value`
-    let number_value = 10.to_value();
+    // ANCHOR: i32
+    // Store `Option<String>` as `Value`
+    let integer_value = 10.to_value();
 
-    // Retrieve `i32` again from `Value`
-    let number = number_value
+    // Retrieve `String` from `Value`
+    let integer = integer_value
         .get::<i32>()
         .expect("The value needs to be of type `i32`.");
-    assert_eq!(number, 10);
+    assert_eq!(integer, 10);
+    // ANCHOR_END: i32
+
+    // ANCHOR: string
+    // Store string as `Value`
+    let string_value = "Hello!".to_value();
+
+    // Retrieve `String` from `Value`
+    let string = string_value
+        .get::<String>()
+        .expect("The value needs to be of type `String`.");
+    assert_eq!(string, "Hello!");
+    // ANCHOR_END: string
+
+    // ANCHOR: string_none
+    // Store `Option<String>` as `Value`
+    let string_none_value = None::<String>.to_value();
+
+    // Retrieve `String` from `Value`
+    let string_none = string_none_value
+        .get::<Option<String>>()
+        .expect("The value needs to be of type `String`.");
+    assert_eq!(string_none, None);
+    // ANCHOR_END: string_none
 }
