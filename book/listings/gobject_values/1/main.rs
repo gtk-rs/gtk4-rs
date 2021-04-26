280b617c1ab3df2fd755b99335ff76be2b28428d
Julian Hofer <julianhofer@gnome.org>
2021-04-26 11:45:14 +0200

    Accomodate changes in glib::Value
    While the listings already build,
    the book itself had to be adapted.

--- book/listings/gobject_values/1/main.rs
+++ book/listings/gobject_values/1/main.rs
@@ -1,18 +1,12 @@
 use gtk::prelude::*;
 
 fn main() {
+    // Convert `i32` to `Value`
     let number_value = 10.to_value();
-    let string_value = "Hello!".to_value();
 
-    // Retrieve `i32` from `number_value`
+    // Retrieve `i32` again from `Value`
     let number = number_value
         .get::<i32>()
         .expect("The value needs to be of type `i32`.");
     assert_eq!(number, 10);
-
-    // Retrieve `String` from `string_value`
-    let string = string_value
-        .get::<String>()
-        .expect("The value needs to be of type `String`.");
-    assert_eq!(string, "Hello!");
 }
