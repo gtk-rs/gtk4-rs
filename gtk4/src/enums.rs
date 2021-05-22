51e578d9b60b8dec0ef0b5e48cfce3f0ebea9027
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-05-23 01:43:50 +0200

    gtk: ignore css_parser_warning_quark
    It should be used as an error domain for CssParserWarning
    but as there's no annotation for that and it's mostly based on a convention that can't be respected for a Warning instead of an Error
    do that manually here.

--- gtk4/src/enums.rs
+++ gtk4/src/enums.rs
@@ -1,6 +1,9 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::Ordering;
+use crate::{CssParserWarning, Ordering};
+use glib::error::ErrorDomain;
+use glib::translate::*;
+use glib::Quark;
 use std::cmp;
 
 impl From<cmp::Ordering> for Ordering {
@@ -22,6 +25,27 @@ impl From<Ordering> for cmp::Ordering {
             Ordering::Larger => Self::Greater,
             Ordering::Smaller => Self::Less,
             Ordering::__Unknown(_) => unreachable!(),
+        }
+    }
+}
+
+impl ErrorDomain for CssParserWarning {
+    fn domain() -> Quark {
+        skip_assert_initialized!();
+        unsafe { from_glib(ffi::gtk_css_parser_warning_quark()) }
+    }
+
+    fn code(self) -> i32 {
+        self.into_glib()
+    }
+
+    fn from(code: i32) -> Option<Self> {
+        skip_assert_initialized!();
+        match code {
+            ffi::GTK_CSS_PARSER_WARNING_DEPRECATED => Some(Self::Deprecated),
+            ffi::GTK_CSS_PARSER_WARNING_SYNTAX => Some(Self::Syntax),
+            ffi::GTK_CSS_PARSER_WARNING_UNIMPLEMENTED => Some(Self::Unimplemented),
+            value => Some(Self::__Unknown(value)),
         }
     }
 }
