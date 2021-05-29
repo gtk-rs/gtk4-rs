a8ccb225ad16be80fa8b369be0037b347c759884
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-05-29 20:45:42 +0200

    tests: drop extern crate

--- gdk4-wayland/tests/check_gir.rs
+++ gdk4-wayland/tests/check_gir.rs
@@ -2,8 +2,6 @@
 // See the COPYRIGHT file at the top-level directory of this distribution.
 // Licensed under the MIT license, see the LICENSE file or <https://opensource.org/licenses/MIT>
 
-extern crate gir_format_check;
-
 #[test]
 fn check_gir_file() {
     let res = gir_format_check::check_gir_file("Gir.toml");
