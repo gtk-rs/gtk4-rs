ec73e9620dd16c5a02d5e4f00c7d8431c34fc59c
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-06-03 11:56:06 +0200

    gdk-wayland: WaylandSurface is not final

--- gdk4-wayland/src/wayland_surface.rs
+++ gdk4-wayland/src/wayland_surface.rs
@@ -1,17 +1,22 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
 use crate::WaylandSurface;
-use glib::translate::ToGlibPtr;
+use glib::translate::*;
+use glib::IsA;
 use wayland_client::protocol::wl_surface::WlSurface;
 use wayland_client::sys::client::wl_proxy;
 use wayland_client::Proxy;
 
-impl WaylandSurface {
+pub trait WaylandSurfaceExtManual: 'static {
     #[doc(alias = "gdk_wayland_surface_get_wl_surface")]
     #[doc(alias = "get_wl_surface")]
-    pub fn wl_surface(&self) -> WlSurface {
+    fn wl_surface(&self) -> WlSurface;
+}
+
+impl<O: IsA<WaylandSurface>> WaylandSurfaceExtManual for O {
+    fn wl_surface(&self) -> WlSurface {
         unsafe {
-            let ptr = ffi::gdk_wayland_surface_get_wl_surface(self.to_glib_none().0);
+            let ptr = ffi::gdk_wayland_surface_get_wl_surface(self.as_ref().to_glib_none().0);
             Proxy::from_c_ptr(ptr as *mut wl_proxy).into()
         }
     }
