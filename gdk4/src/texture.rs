7877ab98f0554f6b5d43a7658e2f416b4ace1907
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-06-15 10:12:36 +0200

    gdk: Texture is actually not final
    it's the parent type of MemoryTexture/GLTexture

--- gdk4/src/texture.rs
+++ gdk4/src/texture.rs
@@ -2,12 +2,17 @@
 
 use crate::Texture;
 use glib::translate::*;
+use glib::IsA;
 
-impl Texture {
+pub trait TextureExtManual: 'static {
     #[doc(alias = "gdk_texture_download")]
-    pub fn download(&self, data: &mut [u8], stride: usize) {
+    fn download(&self, data: &mut [u8], stride: usize);
+}
+
+impl<O: IsA<Texture>> TextureExtManual for O {
+    fn download(&self, data: &mut [u8], stride: usize) {
         unsafe {
-            ffi::gdk_texture_download(self.to_glib_none().0, data.as_mut_ptr(), stride);
+            ffi::gdk_texture_download(self.as_ref().to_glib_none().0, data.as_mut_ptr(), stride);
         }
     }
 }
