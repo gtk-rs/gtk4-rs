92d1bc290e4667470148e61fdc49ed79707569d4
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-07-07 20:29:51 +0400

    x11: manually bind display_get_egl_display
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- gdk4-x11/src/x11_display.rs
+++ gdk4-x11/src/x11_display.rs
@@ -2,9 +2,26 @@
 
 use crate::X11Display;
 use glib::translate::ToGlibPtr;
+#[cfg(any(feature = "v4_4", feature = "dox"))]
+use khronos_egl as egl;
 use x11::xlib;
 
 impl X11Display {
+    #[cfg(any(feature = "v4_4", feature = "dox"))]
+    #[cfg_attr(feature = "dox", doc(cfg(feature = "v4_4")))]
+    #[doc(alias = "gdk_x11_display_get_egl_display")]
+    #[doc(alias = "get_egl_display")]
+    pub fn egl_display(&self) -> Option<egl::Display> {
+        unsafe {
+            let ptr = ffi::gdk_x11_display_get_egl_display(self.to_glib_none().0);
+            if ptr.is_null() {
+                None
+            } else {
+                Some(egl::Display::from_ptr(ptr))
+            }
+        }
+    }
+
     #[doc(alias = "gdk_x11_display_get_xdisplay")]
     #[doc(alias = "get_xdisplay")]
     pub unsafe fn xdisplay(&self) -> *mut xlib::Display {
