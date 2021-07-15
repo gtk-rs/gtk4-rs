f51e652c7327b6bc14f2e83cf9f2a02e715a0dd3
Kelt Rycroft <kelt.rycroft@gmail.com>
2021-07-15 09:31:40 -0700

    fix arg_vec methods in GlShader

--- gsk4/src/gl_shader.rs
+++ gsk4/src/gl_shader.rs
@@ -8,14 +8,14 @@ impl GLShader {
     #[doc(alias = "get_arg_vec2")]
     pub fn arg_vec2(&self, args: &glib::Bytes, idx: i32) -> graphene::Vec2 {
         unsafe {
-            let out_value = std::ptr::null_mut();
+            let mut out_value = graphene::Vec2::uninitialized();
             ffi::gsk_gl_shader_get_arg_vec2(
                 self.to_glib_none().0,
                 args.to_glib_none().0,
                 idx,
-                out_value,
+                out_value.to_glib_none_mut().0,
             );
-            from_glib_none(out_value)
+            out_value
         }
     }
 
@@ -23,14 +23,14 @@ impl GLShader {
     #[doc(alias = "get_arg_vec3")]
     pub fn arg_vec3(&self, args: &glib::Bytes, idx: i32) -> graphene::Vec3 {
         unsafe {
-            let out_value = std::ptr::null_mut();
+            let mut out_value = graphene::Vec3::uninitialized();
             ffi::gsk_gl_shader_get_arg_vec3(
                 self.to_glib_none().0,
                 args.to_glib_none().0,
                 idx,
-                out_value,
+                out_value.to_glib_none_mut().0,
             );
-            from_glib_none(out_value)
+            out_value
         }
     }
 
@@ -38,14 +38,14 @@ impl GLShader {
     #[doc(alias = "get_arg_vec4")]
     pub fn arg_vec4(&self, args: &glib::Bytes, idx: i32) -> graphene::Vec4 {
         unsafe {
-            let out_value = std::ptr::null_mut();
+            let mut out_value = graphene::Vec4::uninitialized();
             ffi::gsk_gl_shader_get_arg_vec4(
                 self.to_glib_none().0,
                 args.to_glib_none().0,
                 idx,
-                out_value,
+                out_value.to_glib_none_mut().0,
             );
-            from_glib_none(out_value)
+            out_value
         }
     }
 }
