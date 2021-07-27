812a177f66e84d7e6d1d30caded98a4ede453a8a
Ivan Molodetskikh <yalterz@gmail.com>
2021-07-27 11:06:42 +0300

    examples: Add a glium GLArea example
    Tested on Linux and on Windows.

---
+++ examples/glium_gl_area/glium_gl_area/mod.rs
@@ -0,0 +1,53 @@
+mod imp;
+
+use gtk::{
+    gdk, glib,
+    prelude::{GLAreaExt, WidgetExt},
+};
+
+glib::wrapper! {
+    pub struct GliumGLArea(ObjectSubclass<imp::GliumGLArea>)
+        @extends gtk::GLArea, gtk::Widget;
+}
+
+impl Default for GliumGLArea {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl GliumGLArea {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create GliumGLArea")
+    }
+}
+
+unsafe impl glium::backend::Backend for GliumGLArea {
+    fn swap_buffers(&self) -> Result<(), glium::SwapBuffersError> {
+        // We're supposed to draw (and hence swap buffers) only inside the `render()` vfunc or
+        // signal, which means that GLArea will handle buffer swaps for us.
+        Ok(())
+    }
+
+    unsafe fn get_proc_address(&self, symbol: &str) -> *const std::ffi::c_void {
+        epoxy::get_proc_addr(symbol)
+    }
+
+    fn get_framebuffer_dimensions(&self) -> (u32, u32) {
+        let scale = self.scale_factor();
+        let width = self.width();
+        let height = self.height();
+        ((width * scale) as u32, (height * scale) as u32)
+    }
+
+    fn is_current(&self) -> bool {
+        match self.context() {
+            Some(context) => gdk::GLContext::current() == Some(context),
+            None => false,
+        }
+    }
+
+    unsafe fn make_current(&self) {
+        GLAreaExt::make_current(self);
+    }
+}
