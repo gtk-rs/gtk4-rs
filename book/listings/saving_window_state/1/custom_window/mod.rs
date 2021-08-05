511195ce43ea78b987c2ff4253ef8045ba2a4ebb
grelltrier <67273701+grelltrier@users.noreply.github.com>
2021-08-05 19:54:00 +0000

    Fixed comments
    I believe "Get" and "Set" was mixed up in the comments explaining this code

--- book/listings/saving_window_state/1/custom_window/mod.rs
+++ book/listings/saving_window_state/1/custom_window/mod.rs
@@ -27,7 +27,7 @@ impl CustomWindow {
         // Get the size of the window
         let size = self.default_size();
 
-        // Get the window state from `settings`
+        // Set the window state in `settings`
         settings.set_int("window-width", size.0)?;
         settings.set_int("window-height", size.1)?;
         settings.set_boolean("is-maximized", self.is_maximized())?;
@@ -39,7 +39,7 @@ impl CustomWindow {
         // Get `settings` from `imp::CustomWindow`
         let settings = &imp::CustomWindow::from_instance(self).settings;
 
-        // Set the window state in `settings`
+        // Get the window state from `settings`
         let width = settings.int("window-width");
         let height = settings.int("window-height");
         let is_maximized = settings.boolean("is-maximized");
