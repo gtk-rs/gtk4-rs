16cac9ffb3643118ea287f35384c88bde21e3bbb
Julian Hofer <julianhofer@gnome.org>
2021-09-19 11:17:53 +0200

    book: Finish up the actions part of the chapter

---
+++ book/listings/actions/5/window/mod.rs
@@ -0,0 +1,19 @@
+mod imp;
+
+use glib::Object;
+use gtk::Application;
+use gtk::{gio, glib};
+
+glib::wrapper! {
+    pub struct Window(ObjectSubclass<imp::Window>)
+        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
+
+impl Window {
+    pub fn new(app: &Application) -> Self {
+        // Create new window
+        Object::new(&[("application", app)]).expect("Failed to create Window")
+    }
+}
