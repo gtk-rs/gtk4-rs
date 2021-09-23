c64a06868c7a0e1f1e3bcd1ec8ee881611e95132
Julian Hofer <julianhofer@gnome.org>
2021-09-23 21:38:11 +0200

    book: Continue with chapter refactoring

--- book/listings/actions/2/main.rs
+++ book/listings/actions/2/main.rs
@@ -1,9 +1,11 @@
 use gio::SimpleAction;
 use glib::clone;
+use gtk::gio::SimpleActionGroup;
 use gtk::prelude::*;
 use gtk::{gio, glib};
-use gtk::{Application, ApplicationWindow, Button};
+use gtk::{Application, ApplicationWindow};
 
+// ANCHOR: main
 fn main() {
     // Create a new application
     let app = Application::builder()
@@ -13,9 +15,13 @@ fn main() {
     // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
 
+    // Set keyboard accelerator to trigger "win.quit".
+    app.set_accels_for_action("win.quit", &["<primary>Q"]);
+
     // Run the application
     app.run();
 }
+// ANCHOR_END: main
 
 // ANCHOR: build_ui
 fn build_ui(app: &Application) {
@@ -32,23 +38,12 @@ fn build_ui(app: &Application) {
     }));
     window.add_action(&action_quit);
 
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |button| {
-        // Activate "win.quit" without passing a parameter
-        button.activate_action("win.quit", None);
-    });
-
-    // Add button
-    window.set_child(Some(&button));
+    // ANCHOR: action_group
+    // Create a new action group and add actions to it
+    let actions = SimpleActionGroup::new();
+    window.insert_action_group("win", Some(&actions));
+    actions.add_action(&action_quit);
+    // ANCHOR_END: action_group
 
     // Present window to the user
     window.present();
