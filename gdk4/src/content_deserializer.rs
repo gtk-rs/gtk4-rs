468101c56ca9aaaa7a136ca5d2dc2b1021e8de98
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-11-08 18:02:42 +0100

    gdk: add nicer variants for Content[Deserializer|Serializer]::value

--- gdk4/src/content_deserializer.rs
+++ gdk4/src/content_deserializer.rs
@@ -2,6 +2,7 @@
 
 use crate::ContentDeserializer;
 use glib::translate::*;
+use glib::value::FromValue;
 
 impl ContentDeserializer {
     pub fn set_value(&self, value: glib::Value) {
@@ -22,6 +23,16 @@ impl ContentDeserializer {
                 self.to_glib_none().0,
             ))
         }
+    }
+
+    // rustdoc-stripper-ignore-next
+    /// Similar to [`Self::value`] but panics if the value is of a different type.
+    #[doc(alias = "gdk_content_deserializer_get_value")]
+    #[doc(alias = "get_value")]
+    pub fn value_as<V: for<'b> FromValue<'b> + 'static>(&self) -> V {
+        self.value()
+            .get_owned::<V>()
+            .expect("Failed to get the value")
     }
 
     #[doc(alias = "gdk_content_deserializer_return_error")]
--- gdk4/src/content_serializer.rs
+++ gdk4/src/content_serializer.rs
@@ -2,6 +2,7 @@
 
 use crate::ContentSerializer;
 use glib::translate::*;
+use glib::value::FromValue;
 
 impl ContentSerializer {
     #[doc(alias = "gdk_content_serializer_get_priority")]
@@ -12,6 +13,16 @@ impl ContentSerializer {
                 self.to_glib_none().0,
             ))
         }
+    }
+
+    // rustdoc-stripper-ignore-next
+    /// Similar to [`Self::value`] but panics if the value is of a different type.
+    #[doc(alias = "gdk_content_serializer_get_value")]
+    #[doc(alias = "get_value")]
+    pub fn value_as<V: for<'b> FromValue<'b> + 'static>(&self) -> V {
+        self.value()
+            .get_owned::<V>()
+            .expect("Failed to get the value")
     }
 
     #[doc(alias = "gdk_content_serializer_return_error")]
