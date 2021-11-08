62ec0754ccffc3af84238cca2700294f2da3b459
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-11-08 17:01:26 +0100

    drop manual annotations
    As those were fixed upstream already

--- gdk4/src/content_formats.rs
+++ gdk4/src/content_formats.rs
@@ -15,18 +15,6 @@ impl ContentFormats {
             FromGlibContainer::from_glib_none_num(types, n_types.assume_init() as usize)
         }
     }
-
-    #[doc(alias = "gdk_content_formats_get_mime_types")]
-    pub fn mime_types(&self) -> Vec<glib::GString> {
-        unsafe {
-            let mut n_mime_types = std::mem::MaybeUninit::uninit();
-            let mime_types = ffi::gdk_content_formats_get_mime_types(
-                self.to_glib_none().0,
-                n_mime_types.as_mut_ptr(),
-            );
-            FromGlibContainer::from_glib_none_num(mime_types, n_mime_types.assume_init() as usize)
-        }
-    }
 }
 
 #[cfg(any(feature = "v4_4", feature = "dox"))]
