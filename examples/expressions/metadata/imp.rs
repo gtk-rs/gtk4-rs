2a4428225ea7a2fa1130ba3b6d5bd728ae43a510
SeaDve <davecruz48@gmail.com>
2021-11-11 17:50:56 +0800

    Add expressions example

---
+++ examples/expressions/metadata/imp.rs
@@ -0,0 +1,81 @@
+use glib::prelude::*;
+use glib::subclass::prelude::*;
+use gtk::glib;
+
+use std::cell::RefCell;
+
+#[derive(Debug)]
+pub struct Metadata {
+    pub title: RefCell<String>,
+    pub last_modified: RefCell<glib::DateTime>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for Metadata {
+    const NAME: &'static str = "Metadata";
+    type Type = super::Metadata;
+    type ParentType = glib::Object;
+
+    fn new() -> Self {
+        let now = glib::DateTime::new_now_local().unwrap();
+
+        Self {
+            title: RefCell::new(String::new()),
+            last_modified: RefCell::new(now),
+        }
+    }
+}
+
+impl ObjectImpl for Metadata {
+    fn properties() -> &'static [glib::ParamSpec] {
+        use once_cell::sync::Lazy;
+        static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
+            vec![
+                glib::ParamSpec::new_string(
+                    "title",
+                    "Title",
+                    "Title of the note",
+                    None,
+                    glib::ParamFlags::READWRITE,
+                ),
+                glib::ParamSpec::new_boxed(
+                    "last-modified",
+                    "Last Modified",
+                    "Last modified datetime of the note",
+                    glib::DateTime::static_type(),
+                    glib::ParamFlags::READWRITE,
+                ),
+            ]
+        });
+
+        PROPERTIES.as_ref()
+    }
+
+    fn set_property(
+        &self,
+        _obj: &Self::Type,
+        _id: usize,
+        value: &glib::Value,
+        pspec: &glib::ParamSpec,
+    ) {
+        match pspec.name() {
+            "title" => {
+                let title = value.get().unwrap();
+                self.title.replace(title);
+            }
+            "last-modified" => {
+                let last_modified = value.get().unwrap();
+                self.last_modified.replace(last_modified);
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn property(&self, _obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+        match pspec.name() {
+            "title" => self.title.borrow().to_value(),
+            "last-modified" => self.last_modified.borrow().to_value(),
+            _ => unimplemented!(),
+        }
+    }
+}
---
+++ examples/expressions/note/imp.rs
@@ -0,0 +1,59 @@
+use glib::prelude::*;
+use glib::subclass::prelude::*;
+use gtk::glib;
+
+use once_cell::unsync::OnceCell;
+
+use super::Metadata;
+
+#[derive(Debug, Default)]
+pub struct Note {
+    pub metadata: OnceCell<Metadata>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for Note {
+    const NAME: &'static str = "Note";
+    type Type = super::Note;
+    type ParentType = glib::Object;
+}
+
+impl ObjectImpl for Note {
+    fn properties() -> &'static [glib::ParamSpec] {
+        use once_cell::sync::Lazy;
+        static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
+            vec![glib::ParamSpec::new_object(
+                "metadata",
+                "Metadata",
+                "Metadata containing info of note",
+                Metadata::static_type(),
+                glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+            )]
+        });
+
+        PROPERTIES.as_ref()
+    }
+
+    fn set_property(
+        &self,
+        _obj: &Self::Type,
+        _id: usize,
+        value: &glib::Value,
+        pspec: &glib::ParamSpec,
+    ) {
+        match pspec.name() {
+            "metadata" => {
+                let metadata = value.get().unwrap();
+                self.metadata.set(metadata).unwrap();
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn property(&self, _obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+        match pspec.name() {
+            "metadata" => self.metadata.get().unwrap().to_value(),
+            _ => unimplemented!(),
+        }
+    }
+}
