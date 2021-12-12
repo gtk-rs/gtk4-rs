09df14f23e5715b50532a2735229a6ff69a97e3d
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-12-12 14:28:26 +0100

    gdk4-x11: create type aliases for XID/XWindow/XCursor
    if the xlib feature is not used, instead of completely ignoring those functions
    that returns a XID (which is an alias of ulong)
    let's provide an alias ourselves in that case and hide it in case the xlib feature is enabled.
    
    This should help people using gdk4-x11 use more features without pulling the x11 crate
    that doesn't add any useful features as is other than wrapping pointers.

--- gdk4-x11/src/x11_surface.rs
+++ gdk4-x11/src/x11_surface.rs
@@ -1,29 +1,22 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
 use crate::X11Surface;
-#[cfg(any(feature = "xlib", feature = "dox"))]
-#[cfg_attr(feature = "dox", doc(cfg(feature = "xlib")))]
+#[cfg(not(feature = "xlib"))]
+use crate::XWindow;
 use glib::translate::*;
 #[cfg(any(feature = "xlib", feature = "dox"))]
 #[cfg_attr(feature = "dox", doc(cfg(feature = "xlib")))]
-use x11::xlib;
+use x11::xlib::Window as XWindow;
 
 impl X11Surface {
-    #[cfg(any(feature = "xlib", feature = "dox"))]
-    #[cfg_attr(feature = "dox", doc(cfg(feature = "xlib")))]
     #[doc(alias = "gdk_x11_surface_get_xid")]
     #[doc(alias = "get_xid")]
-    pub fn xid(&self) -> xlib::Window {
+    pub fn xid(&self) -> XWindow {
         unsafe { ffi::gdk_x11_surface_get_xid(self.to_glib_none().0) }
     }
 
-    #[cfg(any(feature = "xlib", feature = "dox"))]
-    #[cfg_attr(feature = "dox", doc(cfg(feature = "xlib")))]
     #[doc(alias = "gdk_x11_surface_lookup_for_display")]
-    pub fn lookup_for_display(
-        display: &crate::X11Display,
-        window: xlib::Window,
-    ) -> Option<X11Surface> {
+    pub fn lookup_for_display(display: &crate::X11Display, window: XWindow) -> Option<X11Surface> {
         skip_assert_initialized!();
         unsafe {
             from_glib_none(ffi::gdk_x11_surface_lookup_for_display(
