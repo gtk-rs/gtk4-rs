8b10606a674cb7c267363956bb5222e822137339
Maximiliano Sandoval R <msandova@gnome.org>
2021-12-28 11:50:27 +0100

    texture: Add async wrappers

--- gdk4/src/texture.rs
+++ gdk4/src/texture.rs
@@ -31,3 +31,107 @@ impl<O: IsA<Texture>> TextureExtManual for O {
         }
     }
 }
+
+// TODO Figure out how to implement an async variant for for_pixbuf.
+impl Texture {
+    #[cfg(any(feature = "v4_6", feature = "dox"))]
+    #[cfg_attr(feature = "dox", doc(cfg(feature = "v4_6")))]
+    pub async fn from_bytes_future(bytes: &glib::Bytes) -> Result<Texture, glib::Error> {
+        assert_initialized_main_thread!();
+        unsafe {
+            let (sender, receiver) = futures_channel::oneshot::channel();
+
+            let task = gio::Task::new(Texture::NONE, gio::Cancellable::NONE, move |task, _| {
+                let result = task.propagate();
+                let _ = sender.send(result);
+            });
+
+            let task_func = glib::clone!(@strong bytes => move |task: gio::Task<Texture>, _: Option<&Texture>, _: Option<&gio::Cancellable>| {
+                let result = Texture::from_bytes(&bytes);
+                task.return_result(result);
+            });
+
+            task.run_in_thread(task_func);
+
+            receiver.await.unwrap()
+        }
+    }
+
+    pub async fn from_file_future(file: &impl IsA<gio::File>) -> Result<Texture, glib::Error> {
+        assert_initialized_main_thread!();
+        unsafe {
+            let (sender, receiver) = futures_channel::oneshot::channel();
+
+            let task = gio::Task::new(
+                Some(file.as_ref()),
+                gio::Cancellable::NONE,
+                move |task, _: Option<&gio::File>| {
+                    let result = task.propagate();
+                    let _ = sender.send(result);
+                },
+            );
+
+            let task_func = move |task: gio::Task<Texture>,
+                                  source_object: Option<&gio::File>,
+                                  _: Option<&gio::Cancellable>| {
+                let file = source_object.unwrap();
+                let result = Texture::from_file(file);
+                task.return_result(result);
+            };
+
+            task.run_in_thread(task_func);
+
+            receiver.await.unwrap()
+        }
+    }
+
+    #[cfg(any(feature = "v4_6", feature = "dox"))]
+    #[cfg_attr(feature = "dox", doc(cfg(feature = "v4_6")))]
+    pub async fn from_filename_future(
+        path: &'static (impl AsRef<std::path::Path> + std::marker::Sync),
+    ) -> Result<Texture, glib::Error> {
+        assert_initialized_main_thread!();
+        unsafe {
+            let (sender, receiver) = futures_channel::oneshot::channel();
+
+            let task = gio::Task::new(Texture::NONE, gio::Cancellable::NONE, move |task, _| {
+                let result = task.propagate();
+                let _ = sender.send(result);
+            });
+
+            let task_func = move |task: gio::Task<Texture>,
+                                  _: Option<&Texture>,
+                                  _: Option<&gio::Cancellable>| {
+                let result = Texture::from_filename(path);
+                task.return_result(result);
+            };
+
+            task.run_in_thread(task_func);
+
+            receiver.await.unwrap()
+        }
+    }
+
+    pub async fn from_resource_future(resource_path: &'static str) -> Result<Texture, glib::Error> {
+        assert_initialized_main_thread!();
+        unsafe {
+            let (sender, receiver) = futures_channel::oneshot::channel();
+
+            let task = gio::Task::new(Texture::NONE, gio::Cancellable::NONE, move |task, _| {
+                let result = task.propagate();
+                let _ = sender.send(result);
+            });
+
+            let task_func = move |task: gio::Task<Texture>,
+                                  _: Option<&Texture>,
+                                  _: Option<&gio::Cancellable>| {
+                let texture = Texture::from_resource(resource_path);
+                task.return_result(Ok(texture));
+            };
+
+            task.run_in_thread(task_func);
+
+            receiver.await.unwrap()
+        }
+    }
+}
