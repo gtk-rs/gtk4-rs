2586da68e29daffe43e760bf8260c79aa640b649
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-01-01 12:24:38 +0000

    deploy: b1c21a26cd9483a47fff30813f639aca30113853

--- git/docs/gsk4/prelude/sidebar-items.js
+++ git/docs/gsk4/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["GskRendererExt","Trait containing all [`struct@Renderer`] methods."],["GskRendererExtManual","Trait containing manually implemented methods of `Renderer`."],["IsRenderNode","A common trait implemented by the various `RenderNode` types."]]});
+initSidebarItems({"trait":[["GskRendererExt","Trait containing all [`struct@Renderer`] methods."],["IsRenderNode","A common trait implemented by the various `RenderNode` types."]]});
