46cae887f4638e74fe87bdd53f2f28b9970b1fe6
Jason Francis <jafrancis999@gmail.com>
2022-02-20 22:57:57 -0500

    gtk4-macros: Verify missing template children with #[template(string)]

--- gtk4-macros/src/composite_template_derive.rs
+++ gtk4-macros/src/composite_template_derive.rs
@@ -5,6 +5,7 @@ use proc_macro_error::{emit_call_site_error, emit_error};
 use quote::quote;
 use syn::Data;
 
+use std::collections::HashMap;
 use std::string::ToString;
 
 use crate::attribute_parser::*;
@@ -24,6 +25,65 @@ fn gen_set_template(source: &TemplateSource) -> TokenStream {
     }
 }
 
+fn check_template_fields(source: &TemplateSource, fields: &[AttributedField]) {
+    #[allow(unused_assignments)]
+    let xml = match source {
+        TemplateSource::String(template) => template,
+        _ => return,
+    };
+
+    let mut reader = quick_xml::Reader::from_str(xml);
+    let mut buf = Vec::new();
+    let mut ids_left = fields
+        .iter()
+        .map(|field| match field.attr.ty {
+            FieldAttributeType::TemplateChild => (field.id(), field.id_span()),
+        })
+        .collect::<HashMap<_, _>>();
+
+    loop {
+        use quick_xml::events::Event;
+
+        let event = reader.read_event(&mut buf);
+        let elem = match &event {
+            Ok(Event::Start(e)) => Some(e),
+            Ok(Event::Empty(e)) => Some(e),
+            Ok(Event::Eof) => break,
+            Err(e) => {
+                emit_call_site_error!(
+                    "Failed reading template XML at position {}: {:?}",
+                    reader.buffer_position(),
+                    e
+                );
+                break;
+            }
+            _ => None,
+        };
+        if let Some(e) = elem {
+            let name = e.name();
+            if name == b"object" || name == b"template" {
+                let id = e
+                    .attributes()
+                    .find_map(|a| a.ok().and_then(|a| (a.key == b"id").then(|| a)));
+                let id = id.as_ref().and_then(|a| std::str::from_utf8(&a.value).ok());
+                if let Some(id) = id {
+                    ids_left.remove(id);
+                }
+            }
+        }
+
+        buf.clear();
+    }
+
+    if let Some((name, span)) = ids_left.iter().next() {
+        emit_error!(
+            span,
+            "Template child with id `{}` not found in template XML",
+            name
+        );
+    }
+}
+
 fn gen_template_child_bindings(fields: &[AttributedField]) -> TokenStream {
     let crate_ident = crate_ident_new();
 
@@ -33,7 +93,7 @@ fn gen_template_child_bindings(fields: &[AttributedField]) -> TokenStream {
             let ident = &field.ident;
             let mut value_internal = false;
             field.attr.args.iter().for_each(|arg| match arg {
-                FieldAttributeArg::Id(value) => {
+                FieldAttributeArg::Id(value, _) => {
                     value_id = value;
                 }
                 FieldAttributeArg::Internal(internal) => {
@@ -112,6 +172,10 @@ pub fn impl_composite_template(input: &syn::DeriveInput) -> TokenStream {
         }
         None => vec![],
     };
+
+    if let Some(source) = &source {
+        check_template_fields(source, &attributed_fields);
+    }
 
     let template_children = gen_template_child_bindings(&attributed_fields);
     let checks = gen_template_child_type_checks(&attributed_fields);
