77cf24bb2798707bca5439034f87bda089c29f23
Jason Francis <jafrancis999@gmail.com>
2022-03-04 18:16:22 -0500

    gtk4-macros: Add failure tests

---
+++ gtk4-macros/tests/compile-fail/template-callback-duplicate-rest.rs
@@ -0,0 +1,18 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+use gtk::glib;
+use gtk::subclass::prelude::*;
+
+struct Functions {}
+
+#[gtk::template_callbacks]
+impl Functions {
+    #[template_callback(function)]
+    fn after_rest(#[rest] #[rest] _values: &[glib::Value]) {}
+}
+
+fn main() {
+    gtk::init().unwrap();
+    let scope = gtk::BuilderRustScope::new();
+    Functions::add_callbacks_to_scope(&scope);
+}
