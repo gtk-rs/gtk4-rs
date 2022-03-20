2d118180ae9f6c7233755792e204ca830ce426b8
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-03-20 12:37:51 +0000

    deploy: 36aa4108f8e221536e194f62d8ee3be8ea6191f0

--- stable/0.4/docs/gtk4_macros/sidebar-items.js
+++ stable/0.4/docs/gtk4_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["template_callbacks","Attribute macro for creating template callbacks from Rust methods."]],"derive":[["CompositeTemplate","Derive macro for using a composite template in a widget."]]});
+initSidebarItems({"attr":[["template_callbacks","Attribute macro for creating template callbacks from Rust methods."],["test","Attribute macro for declaring GTK tests."]],"derive":[["CompositeTemplate","Derive macro for using a composite template in a widget."]]});
