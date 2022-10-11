175aa8a756beaf82c341fc3da1e91091774e10ce
Maximiliano Sandoval <msandova@gnome.org>
2022-10-11 09:07:29 +0200

    gdk-wayland: Update wayland-client crate to 0.30.0-beta.10

--- gdk4-wayland/src/wayland_monitor.rs
+++ gdk4-wayland/src/wayland_monitor.rs
@@ -3,20 +3,29 @@
 use crate::WaylandMonitor;
 #[cfg(any(feature = "wayland_crate", feature = "dox"))]
 #[cfg_attr(feature = "dox", doc(cfg(feature = "wayland_crate")))]
-use glib::translate::ToGlibPtr;
+use {gdk::prelude::*, glib::translate::ToGlibPtr};
+
 #[cfg(any(feature = "wayland_crate", feature = "dox"))]
 #[cfg_attr(feature = "dox", doc(cfg(feature = "wayland_crate")))]
-use wayland_client::{protocol::wl_output::WlOutput, sys::client::wl_proxy, Proxy};
+use wayland_client::{backend::ObjectId, protocol::wl_output::WlOutput, Proxy};
 
 impl WaylandMonitor {
     #[doc(alias = "gdk_wayland_monitor_get_wl_output")]
     #[doc(alias = "get_wl_output")]
     #[cfg(any(feature = "wayland_crate", feature = "dox"))]
     #[cfg_attr(feature = "dox", doc(cfg(feature = "wayland_crate")))]
-    pub fn wl_output(&self) -> WlOutput {
+    pub fn wl_output(&self) -> Option<WlOutput> {
+        let display = self.display().downcast::<crate::WaylandDisplay>().unwrap();
         unsafe {
-            let ptr = ffi::gdk_wayland_monitor_get_wl_output(self.to_glib_none().0);
-            Proxy::from_c_ptr(ptr as *mut wl_proxy).into()
+            let output_ptr = ffi::gdk_wayland_monitor_get_wl_output(self.to_glib_none().0);
+            if output_ptr.is_null() {
+                None
+            } else {
+                let cnx = display.connection();
+                let id = ObjectId::from_ptr(WlOutput::interface(), output_ptr as *mut _).unwrap();
+
+                WlOutput::from_id(&cnx, id).ok()
+            }
         }
     }
 }
