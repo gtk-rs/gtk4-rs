a9ce7534c322bdc519ab08b238cf3a9dab74b719
Bilal Elmoussaoui <belmouss@redhat.com>
2022-10-12 15:52:13 +0200

    gtk: Manually implement FileChooser::set_current_folder
    To drop the assertion as the folder might not be set correctly in some
    cases without an error being returned
    Which causes the panic described in #1049.
    
    Note, that upstream code has to be changed as well in order to fix
    passing a NULL folder
    which is the cause of the critical mentioned in the issue above.
    
    Fixes #1049

--- gtk4/src/file_chooser.rs
+++ gtk4/src/file_chooser.rs
@@ -9,6 +9,9 @@ use glib::IsA;
 pub trait FileChooserExtManual: 'static {
     #[doc(alias = "gtk_file_chooser_add_choice")]
     fn add_choice(&self, id: &str, label: &str, options: &[(&str, &str)]);
+
+    #[doc(alias = "gtk_file_chooser_set_current_folder")]
+    fn set_current_folder(&self, file: Option<&impl IsA<gio::File>>) -> Result<bool, glib::Error>;
 }
 
 impl<O: IsA<FileChooser>> FileChooserExtManual for O {
@@ -42,6 +45,22 @@ impl<O: IsA<FileChooser>> FileChooserExtManual for O {
                 mut_override(options_ids),
                 mut_override(options_labels),
             );
+        }
+    }
+
+    fn set_current_folder(&self, file: Option<&impl IsA<gio::File>>) -> Result<bool, glib::Error> {
+        unsafe {
+            let mut error = std::ptr::null_mut();
+            let result = from_glib(ffi::gtk_file_chooser_set_current_folder(
+                self.as_ref().to_glib_none().0,
+                file.map(|p| p.as_ref()).to_glib_none().0,
+                &mut error,
+            ));
+            if error.is_null() {
+                Ok(result)
+            } else {
+                Err(from_glib_full(error))
+            }
         }
     }
 }
