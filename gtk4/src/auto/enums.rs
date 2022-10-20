456f5a1c823f473f1d808d21ca311b84b0b51730
Bilal Elmoussaoui <belmouss@redhat.com>
2022-10-20 13:26:26 +0200

    gtk: Generate new v4.10 APIs

--- gtk4/src/auto/enums.rs
+++ gtk4/src/auto/enums.rs
@@ -2298,6 +2298,111 @@ impl ToValue for CellRendererMode {
     }
 }
 
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy)]
+#[non_exhaustive]
+#[doc(alias = "GtkCollation")]
+pub enum Collation {
+    #[doc(alias = "GTK_COLLATION_NONE")]
+    None,
+    #[doc(alias = "GTK_COLLATION_UNICODE")]
+    Unicode,
+    #[doc(alias = "GTK_COLLATION_FILENAME")]
+    Filename,
+    #[doc(hidden)]
+    __Unknown(i32),
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+impl fmt::Display for Collation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Collation::{}",
+            match *self {
+                Self::None => "None",
+                Self::Unicode => "Unicode",
+                Self::Filename => "Filename",
+                _ => "Unknown",
+            }
+        )
+    }
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+#[doc(hidden)]
+impl IntoGlib for Collation {
+    type GlibType = ffi::GtkCollation;
+
+    fn into_glib(self) -> ffi::GtkCollation {
+        match self {
+            Self::None => ffi::GTK_COLLATION_NONE,
+            Self::Unicode => ffi::GTK_COLLATION_UNICODE,
+            Self::Filename => ffi::GTK_COLLATION_FILENAME,
+            Self::__Unknown(value) => value,
+        }
+    }
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+#[doc(hidden)]
+impl FromGlib<ffi::GtkCollation> for Collation {
+    unsafe fn from_glib(value: ffi::GtkCollation) -> Self {
+        skip_assert_initialized!();
+        match value {
+            ffi::GTK_COLLATION_NONE => Self::None,
+            ffi::GTK_COLLATION_UNICODE => Self::Unicode,
+            ffi::GTK_COLLATION_FILENAME => Self::Filename,
+            value => Self::__Unknown(value),
+        }
+    }
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+impl StaticType for Collation {
+    fn static_type() -> Type {
+        unsafe { from_glib(ffi::gtk_collation_get_type()) }
+    }
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+impl glib::value::ValueType for Collation {
+    type Type = Self;
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+unsafe impl<'a> FromValue<'a> for Collation {
+    type Checker = glib::value::GenericValueTypeChecker<Self>;
+
+    unsafe fn from_value(value: &'a glib::Value) -> Self {
+        skip_assert_initialized!();
+        from_glib(glib::gobject_ffi::g_value_get_enum(value.to_glib_none().0))
+    }
+}
+
+#[cfg(any(feature = "v4_10", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v4_10")))]
+impl ToValue for Collation {
+    fn to_value(&self) -> glib::Value {
+        let mut value = glib::Value::for_value_type::<Self>();
+        unsafe {
+            glib::gobject_ffi::g_value_set_enum(value.to_glib_none_mut().0, self.into_glib());
+        }
+        value
+    }
+
+    fn value_type(&self) -> glib::Type {
+        Self::static_type()
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy)]
 #[non_exhaustive]
 #[doc(alias = "GtkConstraintAttribute")]
