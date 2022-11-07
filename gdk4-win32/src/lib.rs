851682f549b0b98b82d33106fcd4aa2dce66e940
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-11-07 23:06:35 +0400

    gdk-win32: implement Win32Display.add_filter()
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- gdk4-win32/src/lib.rs
+++ gdk4-win32/src/lib.rs
@@ -21,6 +21,8 @@ mod auto;
 pub use auto::*;
 
 mod win32_display;
+pub use win32_display::Win32DisplayFilterHandle;
+
 mod win32_hcursor;
 mod win32_surface;
 
@@ -33,7 +35,33 @@ pub struct HICON(pub isize);
 #[cfg(not(feature = "win32"))]
 pub struct HWND(pub isize);
 
+#[cfg(not(feature = "win32"))]
+#[repr(transparent)]
+pub struct WPARAM(pub usize);
+#[cfg(not(feature = "win32"))]
+#[repr(transparent)]
+pub struct LPARAM(pub isize);
+
+#[cfg(not(feature = "win32"))]
+#[repr(C)]
+pub struct POINT {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[allow(non_snake_case)]
+#[cfg(not(feature = "win32"))]
+#[repr(C)]
+pub struct MSG {
+    pub hwnd: HWND,
+    pub message: u32,
+    pub wParam: WPARAM,
+    pub lParam: LPARAM,
+    pub time: u32,
+    pub pt: POINT,
+}
+
 #[cfg(feature = "win32")]
 pub use windows::Win32::Foundation::{HANDLE, HWND};
 #[cfg(feature = "win32")]
-pub use windows::Win32::UI::WindowsAndMessaging::{HCURSOR, HICON};
+pub use windows::Win32::UI::WindowsAndMessaging::{HCURSOR, HICON, MSG};
