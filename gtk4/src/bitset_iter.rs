0e7e668d08d0584cad8700c78f2fde919bd610c8
Andy Russell <arussell123@gmail.com>
2022-12-31 20:18:21 -0500

    document BitsetIter functions

--- gtk4/src/bitset_iter.rs
+++ gtk4/src/bitset_iter.rs
@@ -4,11 +4,27 @@ use crate::Bitset;
 use glib::translate::*;
 use std::marker::PhantomData;
 
+/// An opaque struct for iterating over a [`Bitset`].
+///
+/// While this struct does implement [`Iterator`], the constructor methods yield the first value
+/// separately from iterator. The iterator will only yield subsequent values.
+///
+/// ```
+/// let set = Bitset::new_range(0, 20);
+/// let (mut iter, init_value) = BitsetIter::init_first(&set).unwrap();
+/// assert_eq!(init_value, 0);
+/// assert_eq!(iter.next(), Some(1));
+/// assert_eq!(iter.previous(), Some(0));
+/// ```
 #[derive(Copy, Clone)]
 #[doc(alias = "GtkBitsetIter")]
 pub struct BitsetIter<'a>(ffi::GtkBitsetIter, PhantomData<&'a Bitset>);
 
 impl<'a> BitsetIter<'a> {
+    /// Initializes the iterator pointing at the target value and returns that value.
+    ///
+    /// If the target value is not found, the iterator will point at the next value after it. If no
+    /// value greater than or equal to the target exists in the set, then [`None`] is returned.
     #[doc(alias = "gtk_bitset_iter_init_at")]
     pub fn init_at(set: &'a Bitset, target: u32) -> Option<(Self, u32)> {
         assert_initialized_main_thread!();
@@ -33,6 +49,9 @@ impl<'a> BitsetIter<'a> {
         }
     }
 
+    /// Initializes the iterator pointing at the first value in the set and returns that value.
+    ///
+    /// If the set is empty, [`None`] is returned.
     #[doc(alias = "gtk_bitset_iter_init_first")]
     pub fn init_first(set: &'a Bitset) -> Option<(Self, u32)> {
         assert_initialized_main_thread!();
@@ -55,6 +74,9 @@ impl<'a> BitsetIter<'a> {
         }
     }
 
+    /// Initializes the iterator pointing at the last value and returns that value.
+    ///
+    /// If the set is empty, [`None`] is returned.
     #[doc(alias = "gtk_bitset_iter_init_last")]
     pub fn init_last(set: &'a Bitset) -> Option<(Self, u32)> {
         assert_initialized_main_thread!();
@@ -77,16 +99,23 @@ impl<'a> BitsetIter<'a> {
         }
     }
 
+    /// Returns the current value that the iterator is pointing to.
+    ///
+    /// Returns `0` if the iterator is not valid.
     #[doc(alias = "gtk_bitset_iter_get_value")]
     pub fn value(&self) -> u32 {
         unsafe { ffi::gtk_bitset_iter_get_value(self.to_glib_none().0) }
     }
 
+    /// Returns `true` if the iterator is pointing to valid value.
     #[doc(alias = "gtk_bitset_iter_is_valid")]
     pub fn is_valid(&self) -> bool {
         unsafe { from_glib(ffi::gtk_bitset_iter_is_valid(self.to_glib_none().0)) }
     }
 
+    /// Moves the iterator to the previous value in the set.
+    ///
+    /// If the iterator was already pointing to the first value in the set, [`None`] is returned.
     #[doc(alias = "gtk_bitset_iter_previous")]
     pub fn previous(&mut self) -> Option<u32> {
         unsafe {
