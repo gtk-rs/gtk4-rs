8564a17084f2400d63cd41b0f1d6d4ec7ca427d4
Bilal Elmoussaoui <belmouss@redhat.com>
2023-01-14 11:17:14 +0100

    gtk: Use wrapper! for RecentData

--- gtk4/src/recent_data.rs
+++ gtk4/src/recent_data.rs
@@ -1,85 +1,107 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use glib::translate::*;
-use libc::c_char;
+use glib::{
+    collections::{StrV, StrVItem},
+    translate::*,
+};
+use std::ffi::CStr;
 
-#[doc(alias = "GtkRecentData")]
-pub struct RecentData {
-    display_name: Option<String>,
-    description: Option<String>,
-    mime_type: String,
-    app_name: String,
-    app_exec: String,
-    groups: Vec<String>,
-    is_private: bool,
+glib::wrapper! {
+    #[doc(alias = "GtkRecentData")]
+    pub struct RecentData(BoxedInline<ffi::GtkRecentData>);
+
+    match fn {
+        init => |ptr| init_recent_data(ptr),
+        copy_into => |dest, src| copy_into_recent_data(dest, src),
+        clear => |ptr| clear_recent_data(ptr),
+    }
 }
 
 impl RecentData {
+    pub fn new(
+        display_name: Option<&str>,
+        description: Option<&str>,
+        mime_type: &str,
+        app_name: &str,
+        app_exec: &str,
+        groups: &[&str],
+        is_private: bool,
+    ) -> Self {
+        assert_initialized_main_thread!();
+        unsafe {
+            Self::unsafe_from(ffi::GtkRecentData {
+                display_name: display_name.to_glib_full(),
+                description: description.to_glib_full(),
+                mime_type: mime_type.to_glib_full(),
+                app_name: app_name.to_glib_full(),
+                app_exec: app_exec.to_glib_full(),
+                groups: groups.to_glib_full(),
+                is_private: is_private.into_glib(),
+            })
+        }
+    }
+
     pub fn display_name(&self) -> Option<&str> {
-        self.display_name.as_deref()
+        unsafe {
+            if self.inner.display_name.is_null() {
+                None
+            } else {
+                CStr::from_ptr(self.inner.display_name).to_str().ok()
+            }
+        }
     }
 
     pub fn description(&self) -> Option<&str> {
-        self.description.as_deref()
+        unsafe {
+            if self.inner.description.is_null() {
+                None
+            } else {
+                CStr::from_ptr(self.inner.description).to_str().ok()
+            }
+        }
     }
 
     pub fn mime_type(&self) -> &str {
-        &self.mime_type
+        unsafe { CStr::from_ptr(self.inner.mime_type).to_str().unwrap() }
     }
 
     pub fn app_name(&self) -> &str {
-        &self.app_name
+        unsafe { CStr::from_ptr(self.inner.app_name).to_str().unwrap() }
     }
 
     pub fn app_exec(&self) -> &str {
-        &self.app_exec
+        unsafe { CStr::from_ptr(self.inner.app_exec).to_str().unwrap() }
     }
 
-    pub fn groups(&self) -> &Vec<String> {
-        &self.groups
+    pub fn groups<'a>(&self) -> &'a [StrVItem] {
+        unsafe { StrV::from_glib_borrow(self.inner.groups as *const _) }
     }
 
     pub fn is_private(&self) -> bool {
-        self.is_private
+        unsafe { from_glib(self.inner.is_private) }
     }
 }
 
-#[doc(hidden)]
-impl<'a> ToGlibPtr<'a, *mut ffi::GtkRecentData> for RecentData {
-    #[allow(clippy::type_complexity)]
-    type Storage = (
-        Box<ffi::GtkRecentData>,
-        [Stash<'a, *mut c_char, Option<String>>; 2],
-        [Stash<'a, *mut c_char, String>; 3],
-        Stash<'a, *mut *mut c_char, [String]>,
-    );
+unsafe fn init_recent_data(recent_data: *mut ffi::GtkRecentData) {
+    std::ptr::write(recent_data, std::mem::zeroed());
+}
 
-    fn to_glib_none(&'a self) -> Stash<'a, *mut ffi::GtkRecentData, Self> {
-        let display_name = self.display_name.to_glib_none();
-        let description = self.description.to_glib_none();
-        let mime_type = self.mime_type.to_glib_none();
-        let app_name = self.app_name.to_glib_none();
-        let app_exec = self.app_exec.to_glib_none();
-        let groups = self.groups.to_glib_none();
+unsafe fn copy_into_recent_data(dest: *mut ffi::GtkRecentData, src: *const ffi::GtkRecentData) {
+    init_recent_data(dest);
+    (*dest).display_name = glib::ffi::g_strdup((*src).display_name);
+    (*dest).description = glib::ffi::g_strdup((*src).description);
+    (*dest).mime_type = glib::ffi::g_strdup((*src).mime_type);
+    (*dest).app_name = glib::ffi::g_strdup((*src).app_name);
+    (*dest).app_exec = glib::ffi::g_strdup((*src).app_exec);
+    (*dest).groups = glib::ffi::g_strdupv((*src).groups);
+    (*dest).is_private = (*src).is_private;
+}
 
-        let mut data = Box::new(ffi::GtkRecentData {
-            display_name: display_name.0,
-            description: description.0,
-            mime_type: mime_type.0,
-            app_name: app_name.0,
-            app_exec: app_exec.0,
-            groups: groups.0,
-            is_private: self.is_private.into_glib(),
-        });
-
-        Stash(
-            &mut *data,
-            (
-                data,
-                [display_name, description],
-                [mime_type, app_name, app_exec],
-                groups,
-            ),
-        )
-    }
+unsafe fn clear_recent_data(recent_data: *mut ffi::GtkRecentData) {
+    glib::ffi::g_free((*recent_data).display_name as *mut _);
+    glib::ffi::g_free((*recent_data).description as *mut _);
+    glib::ffi::g_free((*recent_data).mime_type as *mut _);
+    glib::ffi::g_free((*recent_data).app_name as *mut _);
+    glib::ffi::g_free((*recent_data).app_exec as *mut _);
+    glib::ffi::g_strfreev((*recent_data).groups as *mut _);
 }
