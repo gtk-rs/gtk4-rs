56bcf1dd70649619bed7a84ada23920ff3289431
Bilal Elmoussaoui <belmouss@redhat.com>
2023-02-06 11:58:07 +0100

    gtk4-macros: Document blueprint support

--- gtk4-macros/src/lib.rs
+++ gtk4-macros/src/lib.rs
@@ -88,6 +88,65 @@ use syn::{parse_macro_input, DeriveInput};
 ///     }
 /// }
 /// ```
+///
+/// The [`CompositeTemplate`] macro can also be used with [Blueprint](https://jwestman.pages.gitlab.gnome.org/blueprint-compiler/)
+///
+/// ```rust
+/// ```no_run
+/// # fn main() {}
+/// use gtk::prelude::*;
+/// use gtk::glib;
+/// use gtk::CompositeTemplate;
+/// use gtk::subclass::prelude::*;
+///
+/// mod imp {
+///     #[derive(Debug, Default, CompositeTemplate)]
+///     #[template(string = "
+///     template MyWidget : Widget {
+///         Label label {
+///             label: 'foobar';
+///         }
+///
+///         Label my_label2 {
+///             label: 'foobaz';
+///         }
+///     }
+///     ")]
+///     pub struct MyWidget {
+///         #[template_child]
+///         pub label: TemplateChild<gtk::Label>,
+///         #[template_child(id = "my_label2")]
+///         pub label2: gtk::TemplateChild<gtk::Label>,
+///     }
+///
+///     #[glib::object_subclass]
+///     impl ObjectSubclass for MyWidget {
+///         const NAME: &'static str = "MyWidget";
+///         type Type = super::MyWidget;
+///         type ParentType = gtk::Widget;
+///         fn class_init(klass: &mut Self::Class) {
+///             klass.bind_template();
+///         }
+///         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+///             obj.init_template();
+///         }
+///     }
+///
+///     impl ObjectImpl for MyWidget {
+///         fn dispose(&self) {
+///             while let Some(child) = self.obj().first_child() {
+///                 child.unparent();
+///             }
+///         }
+///     }
+///     impl WidgetImpl for MyWidget {}
+/// }
+///
+/// glib::wrapper! {
+///     pub struct MyWidget(ObjectSubclass<imp::MyWidget>) @extends gtk::Widget;
+/// }
+/// ```
+///
 #[proc_macro_derive(CompositeTemplate, attributes(template, template_child))]
 #[proc_macro_error]
 pub fn composite_template_derive(input: TokenStream) -> TokenStream {
