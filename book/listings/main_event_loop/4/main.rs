3d807fa81a3791d784a38907cf4cdc9796fe85e3
Luis P Ruiz <78879397+lupinx2@users.noreply.github.com>
2023-05-13 09:05:36 -0700

    Update book/listings/main_event_loop/4/main.rs
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>

--- book/listings/main_event_loop/4/main.rs
+++ book/listings/main_event_loop/4/main.rs
@@ -26,7 +26,7 @@ fn build_ui(app: &Application) {
         .build();
 
     // ANCHOR: callback
-    let (sender, receiver) = MainContext::channel(Priority::DEFAULT);
+    let (sender, receiver) = MainContext::channel(Priority::default());
     // Connect to "clicked" signal of `button`
     button.connect_clicked(move |_| {
         let main_context = MainContext::default();
