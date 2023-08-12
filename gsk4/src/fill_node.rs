1870f233f4d4199aacd6f754b6eb172098a7868b
Bilal Elmoussaoui <belmouss@redhat.com>
2023-08-12 13:53:10 +0200

    gsk: Handle new render nodes

---
+++ gsk4/src/fill_node.rs
@@ -0,0 +1,15 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+use crate::{FillNode, RenderNodeType};
+
+define_render_node!(FillNode, ffi::GskFillNode, RenderNodeType::FillNode);
+
+impl std::fmt::Debug for FillNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("FillNode")
+            .field("child", &self.child())
+            .field("fill_rule", &self.fill_rule())
+            .field("path", &self.path())
+            .finish()
+    }
+}
---
+++ gsk4/src/stroke_node.rs
@@ -0,0 +1,15 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+use crate::{RenderNodeType, StrokeNode};
+
+define_render_node!(StrokeNode, ffi::GskStrokeNode, RenderNodeType::StrokeNode);
+
+impl std::fmt::Debug for StrokeNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("StrokeNode")
+            .field("child", &self.child())
+            .field("stroke", &self.stroke())
+            .field("path", &self.path())
+            .finish()
+    }
+}
