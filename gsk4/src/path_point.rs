a264915bbdda8831d40bef342c77a64a4d48c6c4
Bilal Elmoussaoui <belmouss@redhat.com>
2023-08-25 22:13:54 +0200

    gsk: Adapt to PathPoint.get_curvature api changes

--- gsk4/src/path_point.rs
+++ gsk4/src/path_point.rs
@@ -1,17 +1,22 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::{Path, PathPoint};
+use crate::{Path, PathDirection, PathPoint};
 use glib::translate::*;
 
 impl PathPoint {
     #[doc(alias = "gsk_path_point_get_curvature")]
     #[doc(alias = "get_curvature")]
-    pub fn curvature(&self, path: &Path) -> (f32, Option<graphene::Point>) {
+    pub fn curvature(
+        &self,
+        path: &Path,
+        direction: PathDirection,
+    ) -> (f32, Option<graphene::Point>) {
         unsafe {
             let mut center = graphene::Point::uninitialized();
             let ret = ffi::gsk_path_point_get_curvature(
                 self.to_glib_none().0,
                 path.to_glib_none().0,
+                direction.into_glib(),
                 center.to_glib_none_mut().0,
             );
 
