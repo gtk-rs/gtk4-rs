b3132cc69cc99c7b106b45e66c292416bf452e20
Bilal Elmoussaoui <belmouss@redhat.com>
2024-01-28 10:54:29 +0100

    gdk: Fix SurfaceSize implementation
    It is similar to ToplevelSize type, it is not a boxed type

--- gdk4/src/drag_surface_size.rs
+++ gdk4/src/drag_surface_size.rs
@@ -1,22 +1,22 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+use std::ptr::NonNull;
+
 use glib::translate::*;
 
-glib::wrapper! {
-    #[doc(alias = "GdkDragSurfaceSize")]
-    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub struct DragSurfaceSize(Boxed<ffi::GdkDragSurfaceSize>);
+#[repr(transparent)]
+#[doc(alias = "GdkDragSurfaceSize")]
+pub struct DragSurfaceSize(NonNull<ffi::GdkDragSurfaceSize>);
 
-    match fn {
-        copy => |ptr| glib::gobject_ffi::g_boxed_copy(ffi::gdk_drag_surface_size_get_type(), ptr as *mut _) as *mut ffi::GdkDragSurfaceSize,
-        free => |ptr| glib::gobject_ffi::g_boxed_free(ffi::gdk_drag_surface_size_get_type(), ptr as *mut _),
-        type_ => || ffi::gdk_drag_surface_size_get_type(),
+impl glib::StaticType for DragSurfaceSize {
+    fn static_type() -> glib::Type {
+        unsafe { from_glib(ffi::gdk_drag_surface_size_get_type()) }
     }
 }
 
 impl DragSurfaceSize {
     #[doc(alias = "gdk_drag_surface_size_set_size")]
     pub fn set_size(&self, width: i32, height: i32) {
-        unsafe { ffi::gdk_drag_surface_size_set_size(self.to_glib_none().0, width, height) }
+        unsafe { ffi::gdk_drag_surface_size_set_size(self.0.as_ptr(), width, height) }
     }
 }
