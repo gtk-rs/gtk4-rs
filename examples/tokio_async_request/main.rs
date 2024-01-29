88db796c36b530c52650c408e722e0e53a455273
Claudio Silva Junior <42524939+Claudio-code@users.noreply.github.com>
2024-01-29 14:27:46 -0400

    Examples: async request no blocking main thread (#1578)
    * examples: async request no blocking main thread
    
    * examples: formatted the example code
    
    * examples: added step to install openssl
    
    * examples: added step to install openssl
    
    * examples: made the changes requested in the pr
    
    * examples: removed step
    
    * examples: formatted the example code
    
    * examples: changed gif
    
    * examples: changed features declaration
    
    * examples: changed features declaration
    
    * examples: formatted the example code
    
    * Update examples/Cargo.toml
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>
    
    * examples: Removed new() and implemented Default
    
    * examples: Removed once_cell to use OnceLock
    
    ---------
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>

---
+++ examples/tokio_async_request/main.rs
@@ -0,0 +1,110 @@
+use glib::clone;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+use serde::Deserialize;
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+
+#[derive(Deserialize, Debug)]
+pub struct Pokemon {
+    pub name: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ResultBody {
+    results: Vec<Pokemon>,
+}
+
+#[derive(Debug)]
+pub struct PokemonClient {
+    page: u32,
+}
+
+impl Default for PokemonClient {
+    fn default() -> Self {
+        Self { page: 0 }
+    }
+}
+
+impl PokemonClient {
+    const URI: &'static str = "https://pokeapi.co/api/v2/pokemon";
+    const PAGE_LIMIT: &'static str = "100";
+
+    pub async fn get_pokemon_list(&mut self) -> Result<Vec<Pokemon>, reqwest::Error> {
+        let current_offset = self.page.to_string();
+        let params: [(&str, &str); 2] = [
+            ("limit", Self::PAGE_LIMIT),
+            ("offset", current_offset.as_str()),
+        ];
+        let url = reqwest::Url::parse_with_params(Self::URI, params).unwrap();
+        let body = reqwest::get(url).await?.json::<ResultBody>().await?;
+
+        self.page += 10;
+        Ok(body.results)
+    }
+}
+
+fn runtime() -> &'static Runtime {
+    static RUNTIME: OnceLock<Runtime> = OnceLock::new();
+    RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
+}
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("org.gtk_rs.pokemon.list")
+        .build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let (sender, receiver) = async_channel::bounded::<Result<Vec<Pokemon>, reqwest::Error>>(1);
+    runtime().spawn(clone!(@strong sender => async move {
+        let mut pokemon_client = PokemonClient::default();
+        let pokemon_vec = pokemon_client.get_pokemon_list().await;
+        sender.send(pokemon_vec).await.expect("The channel needs to be open.");
+    }));
+
+    let list_box = gtk::ListBox::builder().build();
+    let scrolled_window = gtk::ScrolledWindow::builder()
+        .hscrollbar_policy(gtk::PolicyType::Never)
+        .min_content_width(360)
+        .child(&list_box)
+        .build();
+
+    scrolled_window.connect_edge_reached(move |_, position| {
+        let mut pokemon_client = PokemonClient::default();
+        if gtk::PositionType::Bottom == position {
+            runtime().spawn(clone!(@strong sender => async move {
+                let pokemon_vec = pokemon_client.get_pokemon_list().await;
+                sender.send(pokemon_vec).await.expect("The channel needs to be open.");
+            }));
+        }
+    });
+
+    glib::spawn_future_local(async move {
+        while let Ok(response) = receiver.recv().await {
+            match response {
+                Ok(response) => {
+                    for pokemon in response {
+                        let label = gtk::Label::new(Some(&pokemon.name));
+                        label.set_halign(gtk::Align::Start);
+                        list_box.append(&label);
+                    }
+                }
+                Err(_) => {
+                    println!("bad request");
+                }
+            }
+        }
+    });
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Tokio integration")
+        .default_width(600)
+        .default_height(300)
+        .child(&scrolled_window)
+        .build();
+    window.present();
+}
