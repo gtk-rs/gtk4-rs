d48338fc77121bfbcbd3cc0a136059f73e502461
jobale <joceran.faugier@gmail.com>
2024-01-29 19:26:44 +0100

    Examples: Dialog's response signal handling (#1514)
    * example of a dialog's response signal handling
    
    * Apply suggestions Remove comment and one-time use statements
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>
    
    * Remove one-time use statement
    
    * Remove useless part of ObjectImpl implement statement.
    
    * Add gtk::Dialog in ui xml file. Remove old instance from code.
    Allow deprecated use of gtk::Dialog.
    
    * Apply suggestions group import, remove unused comment and blank lines.
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>
    
    * Replace redundant statements as suggested.
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>
    
    * Rename gtk::Dialog. Fix multiple identical call in match statement.
    
    * Comment details about types used in callback handler for signal response.
    
    * Rename callback handler popup_dialog in on_counter_notify
    
    * Update examples/composite_dialog/my_app_window/mod.rs
    
    Co-authored-by: jobale <joceran.faugier@gmail.com>
    
    * Update examples/composite_dialog/my_app_window/mod.rs
    
    ---------
    
    Co-authored-by: Bilal Elmoussaoui <belmouss@redhat.com>

---
+++ examples/composite_dialog/README.md
@@ -0,0 +1,6 @@
+# Composite Template Dialog
+
+This example shows the use of callbacks, custom and bind property, notify and response signal in a composite
+template situation.
+
+![Screenshot](screenshot.png)
---
+++ examples/composite_dialog/my_app_window/imp.rs
@@ -0,0 +1,41 @@
+use std::cell::Cell;
+
+use gtk::{glib, prelude::*, subclass::prelude::*};
+
+#[derive(Default, gtk::CompositeTemplate, glib::Properties)]
+#[template(file = "window.ui")]
+#[properties(wrapper_type = super::MyAppWindow)]
+pub struct MyAppWindow {
+    #[property(get, set)]
+    counter: Cell<i32>,
+    #[template_child]
+    pub count_label: TemplateChild<gtk::Label>,
+    #[template_child]
+    pub plus: TemplateChild<gtk::Button>,
+    #[template_child]
+    pub minus: TemplateChild<gtk::Button>,
+    #[template_child]
+    pub dialog: TemplateChild<gtk::Dialog>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for MyAppWindow {
+    const NAME: &'static str = "MyAppWindow";
+    type Type = super::MyAppWindow;
+    type ParentType = gtk::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+        klass.bind_template_instance_callbacks();
+    }
+
+    fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+
+#[glib::derived_properties]
+impl ObjectImpl for MyAppWindow {}
+impl WidgetImpl for MyAppWindow {}
+impl WindowImpl for MyAppWindow {}
+impl ApplicationWindowImpl for MyAppWindow {}
