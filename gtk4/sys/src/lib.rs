71e938c214582b1d076db9181efa84cdc3880542
Bilal Elmoussaoui <belmouss@redhat.com>
2024-01-31 10:44:07 +0100

    Regenerate with latest gir/gir-files
    Also switch to using cargo worskpaces

--- gtk4/sys/src/lib.rs
+++ gtk4/sys/src/lib.rs
@@ -11,6 +11,16 @@
 )]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+use cairo_sys as cairo;
+use gdk4_sys as gdk;
+use gdk_pixbuf_sys as gdk_pixbuf;
+use gio_sys as gio;
+use glib_sys as glib;
+use gobject_sys as gobject;
+use graphene_sys as graphene;
+use gsk4_sys as gsk;
+use pango_sys as pango;
+
 #[allow(unused_imports)]
 use libc::{
     c_char, c_double, c_float, c_int, c_long, c_short, c_uchar, c_uint, c_ulong, c_ushort, c_void,
