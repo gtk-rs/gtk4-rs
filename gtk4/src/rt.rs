bc35182fad9f1f80d05825b2d2f38a045f78f70b
Franz Dietrich <dietrich@teilgedanken.de>
2024-03-27 16:44:57 +0100

    make `thread_local` `IS_MAIN_THREAD` `const` in `rt.rs`

--- gtk4/src/rt.rs
+++ gtk4/src/rt.rs
@@ -13,7 +13,7 @@ extern "C" {
 }
 
 thread_local! {
-    static IS_MAIN_THREAD: Cell<bool> = Cell::new(false)
+    static IS_MAIN_THREAD: Cell<bool> = const{Cell::new(false)}
 }
 
 static INITIALIZED: AtomicBool = AtomicBool::new(false);
