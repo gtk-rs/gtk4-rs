19aa9f2c1d7b62557e04acfe5330744aedab7d18
Jonathan <vanillajonathan@users.noreply.github.com>
2024-04-05 02:01:06 +0200

    Update bookmark_list.rs

--- gtk4/src/bookmark_list.rs
+++ gtk4/src/bookmark_list.rs
@@ -1,12 +1,13 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::BookmarkList;
 use glib::translate::*;
+
+use crate::BookmarkList;
 
 /// # Example
 /// To iterate over `BookmarkList` call `iter::<gio::FileInfo>` to get an iterator.
 /// ```no_run
-/// let bookmarks = BookmarkList::new(Some("bookmarks.xbel"), None);
+/// let bookmarks = crate::BookmarkList::new(Some("bookmarks.xbel"), None);
 /// while let Some(Ok(bookmark)) = bookmarks.iter::<gio::FileInfo>().next() {
 ///     // ...
 /// }
