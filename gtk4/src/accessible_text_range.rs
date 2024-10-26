4a34051cfbff6de671dc995911e488a5bd7b132e
Fina Wilke <code@felinira.net>
2024-10-26 17:10:44 +0200

    gtk/accessible_text_range: Convert to boxed object to make it usable for implementing AccessibleText

--- gtk4/src/accessible_text_range.rs
+++ gtk4/src/accessible_text_range.rs
@@ -1,17 +1,33 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-#[derive(Copy, Clone)]
-#[doc(alias = "GtkAccessibleTextRange")]
-#[repr(transparent)]
-pub struct AccessibleTextRange(crate::ffi::GtkAccessibleTextRange);
+use crate::ffi;
+use glib::translate::*;
+
+glib::wrapper! {
+    #[doc(alias = "GtkAccessibleTextRange")]
+    pub struct AccessibleTextRange(BoxedInline<ffi::GtkAccessibleTextRange>);
+}
 
 impl AccessibleTextRange {
+    pub fn new(start: usize, length: usize) -> Self {
+        skip_assert_initialized!();
+        unsafe { AccessibleTextRange::unsafe_from(ffi::GtkAccessibleTextRange { start, length }) }
+    }
+
     pub fn start(&self) -> usize {
-        self.0.start
+        self.inner.start
+    }
+
+    pub fn set_start(&mut self, start: usize) {
+        self.inner.start = start;
     }
 
     pub fn length(&self) -> usize {
-        self.0.length
+        self.inner.length
+    }
+
+    pub fn set_length(&mut self, length: usize) {
+        self.inner.length = length
     }
 }
 
