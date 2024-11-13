1edfc9856a5cf5906db60127a1be0536f28493f5
Bilal Elmoussaoui <belmouss@redhat.com>
2024-11-13 07:59:48 +0100

    Add a basic gdk4-macos

---
+++ gdk4-macos/src/rt.rs
@@ -0,0 +1,10 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+/// No-op.
+macro_rules! skip_assert_initialized {
+    () => {};
+}
