aaf88950eec68c5240a54d335a671d875c639168
Bilal Elmoussaoui <belmouss@redhat.com>
2024-11-13 07:59:48 +0100

    Add a basic gdk4-macos

---
+++ gdk4-macos/src/prelude.rs
@@ -0,0 +1,7 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
+// rustdoc-stripper-ignore-next
+//! Traits intended for blanket imports.
+
+#[doc(hidden)]
+pub use gdk::prelude::*;
