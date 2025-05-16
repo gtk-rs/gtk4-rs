36f48920bab7410053ae5da2cc79245375ed9ee1
Bilal Elmoussaoui <belmouss@redhat.com>
2025-05-16 23:17:31 +0200

    Fix clippy warnings

--- gtk4/src/tree_store.rs
+++ gtk4/src/tree_store.rs
@@ -102,7 +102,7 @@ impl TreeStore {
                 "Incorrect `new_order` slice length. Expected `{count}`, found `{}`.",
                 new_order.len()
             );
-            let safe_values = new_order.iter().max().map_or(true, |&max| {
+            let safe_values = new_order.iter().max().is_none_or(|&max| {
                 let max = max as i32;
                 max >= 0 && max < count
             });
