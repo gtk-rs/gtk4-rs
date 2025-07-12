58acbaed188507bf6186fe8132668f85c0766342
Bilal Elmoussaoui <belmouss@redhat.com>
2025-07-12 09:45:07 +0200

    gsk: Manually implement Path.foreach_intersection

--- gsk4/src/path.rs
+++ gsk4/src/path.rs
@@ -3,6 +3,8 @@
 use glib::translate::*;
 
 use crate::{ffi, Path, PathForeachFlags, PathOperation};
+#[cfg(feature = "v4_20")]
+use crate::{PathIntersection, PathPoint};
 
 impl Path {
     #[doc(alias = "gsk_path_foreach")]
@@ -32,6 +34,47 @@ impl Path {
             from_glib(ffi::gsk_path_foreach(
                 self.to_glib_none().0,
                 flags.into_glib(),
+                func,
+                super_callback0 as *mut _ as *mut _,
+            ))
+        }
+    }
+
+    #[cfg(feature = "v4_20")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v4_20")))]
+    #[doc(alias = "gsk_path_foreach_intersection")]
+    pub fn foreach_intersection<
+        P: FnMut(&Path, &PathPoint, &Path, &PathPoint, PathIntersection) -> bool,
+    >(
+        &self,
+        path2: Option<&Path>,
+        func: P,
+    ) -> bool {
+        let mut func_data: P = func;
+        unsafe extern "C" fn func_func<
+            P: FnMut(&Path, &PathPoint, &Path, &PathPoint, PathIntersection) -> bool,
+        >(
+            path1: *mut ffi::GskPath,
+            point1: *const ffi::GskPathPoint,
+            path2: *mut ffi::GskPath,
+            point2: *const ffi::GskPathPoint,
+            kind: ffi::GskPathIntersection,
+            user_data: glib::ffi::gpointer,
+        ) -> glib::ffi::gboolean {
+            let path1 = from_glib_borrow(path1);
+            let point1 = from_glib_borrow(point1);
+            let path2 = from_glib_borrow(path2);
+            let point2 = from_glib_borrow(point2);
+            let kind = from_glib(kind);
+            let callback = user_data as *mut P;
+            (*callback)(&path1, &point1, &path2, &point2, kind).into_glib()
+        }
+        let func = Some(func_func::<P> as _);
+        let super_callback0: &mut P = &mut func_data;
+        unsafe {
+            from_glib(ffi::gsk_path_foreach_intersection(
+                self.to_glib_none().0,
+                path2.to_glib_none().0,
                 func,
                 super_callback0 as *mut _ as *mut _,
             ))
