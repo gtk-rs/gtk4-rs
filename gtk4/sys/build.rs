014caf367833cdc62a26be31bfbd0eeb6275f3f8
Sebastian Dröge <sebastian@centricular.com>
2025-11-26 11:12:10 +0200

    Regenerate with latest gir

--- gtk4/sys/build.rs
+++ gtk4/sys/build.rs
@@ -2,16 +2,14 @@
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 
-#[cfg(not(docsrs))]
-use std::process;
-
-#[cfg(docsrs)]
-fn main() {} // prevent linking libraries to avoid documentation failure
-
-#[cfg(not(docsrs))]
 fn main() {
+    if std::env::var("DOCS_RS").is_ok() {
+        // prevent linking libraries to avoid documentation failure
+        return;
+    }
+
     if let Err(s) = system_deps::Config::new().probe() {
         println!("cargo:warning={s}");
-        process::exit(1);
+        std::process::exit(1);
     }
 }
