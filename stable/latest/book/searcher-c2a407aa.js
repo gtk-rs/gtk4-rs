3fffc99ac4dc833761bd50ebb2793950a6c69867
Hofer-Julian <Hofer-Julian@users.noreply.github.com>
2026-01-26 07:30:17 +0000

    deploy: 2e473b3a593b5e79275ee8d4671bf3596c044c30

--- stable/latest/book/searcher-c2a407aa.js
+++ stable/latest/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-15875aa5.js',
+                path_to_root + 'searchindex-41c2b1ae.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
