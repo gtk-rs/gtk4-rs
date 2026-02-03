f9e433e771ebf1f443da9e70a68b7bdacafa4b58
Sebastian Dröge <sebastian@centricular.com>
2026-02-03 12:25:09 +0200

    gtk4: Remove bindings for removed 4.22 application save/restore APIs

--- gtk4/src/subclass/application_window.rs
+++ gtk4/src/subclass/application_window.rs
@@ -5,69 +5,15 @@
 
 use crate::{ApplicationWindow, prelude::*, subclass::prelude::*};
 
-#[cfg(feature = "v4_22")]
-use crate::ffi;
-
-#[cfg(feature = "v4_22")]
-use glib::translate::*;
-
 pub trait ApplicationWindowImpl:
     WindowImpl
     + ObjectSubclass<Type: IsA<ApplicationWindow> + IsA<gio::ActionGroup> + IsA<gio::ActionMap>>
     + 'static
 {
-    #[cfg(feature = "v4_22")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "v4_22")))]
-    fn save_state(&self, state: &glib::VariantDict) -> bool {
-        self.parent_save_state(state)
-    }
 }
 
-pub trait ApplicationWindowImplExt: ApplicationWindowImpl {
-    #[cfg(feature = "v4_22")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "v4_22")))]
-    fn parent_save_state(&self, state: &glib::VariantDict) -> bool {
-        unsafe {
-            let data = Self::type_data();
-            let parent_class = data.as_ref().parent_class() as *mut ffi::GtkApplicationWindowClass;
-            if let Some(f) = (*parent_class).save_state {
-                from_glib(f(
-                    self.obj()
-                        .unsafe_cast_ref::<ApplicationWindow>()
-                        .to_glib_none()
-                        .0,
-                    state.to_glib_none().0,
-                ))
-            } else {
-                false
-            }
-        }
-    }
-}
+pub trait ApplicationWindowImplExt: ApplicationWindowImpl {}
 
 impl<T: ApplicationWindowImpl> ApplicationWindowImplExt for T {}
 
-unsafe impl<T: ApplicationWindowImpl> IsSubclassable<T> for ApplicationWindow {
-    fn class_init(class: &mut ::glib::Class<Self>) {
-        Self::parent_class_init::<T>(class);
-
-        #[cfg(feature = "v4_22")]
-        {
-            let klass = class.as_mut();
-            klass.save_state = Some(application_window_save_state::<T>);
-        }
-    }
-}
-
-#[cfg(feature = "v4_22")]
-unsafe extern "C" fn application_window_save_state<T: ApplicationWindowImpl>(
-    ptr: *mut ffi::GtkApplicationWindow,
-    state: *mut glib::ffi::GVariantDict,
-) -> glib::ffi::gboolean {
-    unsafe {
-        let instance = &*(ptr as *mut T::Instance);
-        let imp = instance.imp();
-
-        imp.save_state(&from_glib_borrow(state)).into_glib()
-    }
-}
+unsafe impl<T: ApplicationWindowImpl> IsSubclassable<T> for ApplicationWindow {}
