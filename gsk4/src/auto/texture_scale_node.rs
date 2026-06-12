ab2100f15e71a2d08aaddbe3630685c077319c45
Bilal Elmoussaoui <belmouss@redhat.com>
2026-06-12 19:55:37 +0200

    gsk: manually implment RectSnap
    As it can't be automated given that is a type alias...

--- gsk4/src/auto/texture_scale_node.rs
+++ gsk4/src/auto/texture_scale_node.rs
@@ -2,9 +2,6 @@
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 
-#[cfg(feature = "v4_24")]
-#[cfg_attr(docsrs, doc(cfg(feature = "v4_24")))]
-use crate::RectSnap;
 use crate::{ScalingFilter, ffi};
 use glib::{prelude::*, translate::*};
 
@@ -49,14 +46,6 @@ impl TextureScaleNode {
                 self.to_glib_none().0,
             ))
         }
-    }
-
-    #[cfg(feature = "v4_24")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "v4_24")))]
-    #[doc(alias = "gsk_texture_scale_node_get_snap")]
-    #[doc(alias = "get_snap")]
-    pub fn snap(&self) -> RectSnap {
-        unsafe { ffi::gsk_texture_scale_node_get_snap(self.to_glib_none().0) }
     }
 
     #[doc(alias = "gsk_texture_scale_node_get_texture")]
--- gsk4/src/repeat_node.rs
+++ gsk4/src/repeat_node.rs
@@ -8,6 +8,32 @@ define_render_node!(
     RenderNodeType::RepeatNode
 );
 
+impl RepeatNode {
+    #[cfg(feature = "v4_24")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v4_24")))]
+    #[doc(alias = "gsk_repeat_node_get_child_snap")]
+    #[doc(alias = "get_child_snap")]
+    pub fn child_snap(&self) -> crate::RectSnap {
+        unsafe {
+            glib::translate::from_glib(crate::ffi::gsk_repeat_node_get_child_snap(
+                glib::translate::ToGlibPtr::to_glib_none(self).0,
+            ))
+        }
+    }
+
+    #[cfg(feature = "v4_24")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v4_24")))]
+    #[doc(alias = "gsk_repeat_node_get_snap")]
+    #[doc(alias = "get_snap")]
+    pub fn snap(&self) -> crate::RectSnap {
+        unsafe {
+            glib::translate::from_glib(crate::ffi::gsk_repeat_node_get_snap(
+                glib::translate::ToGlibPtr::to_glib_none(self).0,
+            ))
+        }
+    }
+}
+
 impl std::fmt::Debug for RepeatNode {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         f.debug_struct("RepeatNode")
