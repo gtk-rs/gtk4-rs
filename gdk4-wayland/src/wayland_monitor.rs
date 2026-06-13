d5f5b88cd41032c8e0e7e93ae00000cebe3656db
Bilal Elmoussaoui <belmouss@redhat.com>
2026-06-13 23:14:51 +0200

    gdk-wayland: Add _raw wrappers
    Fixes #2071

--- gdk4-wayland/src/wayland_monitor.rs
+++ gdk4-wayland/src/wayland_monitor.rs
@@ -1,34 +1,37 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+use std::ffi::c_void;
+use std::ptr::NonNull;
+
 #[cfg(feature = "wayland_crate")]
 #[cfg_attr(docsrs, doc(cfg(feature = "wayland_crate")))]
 use wayland_client::{Proxy, backend::ObjectId, protocol::wl_output::WlOutput};
+
 #[cfg(feature = "wayland_crate")]
 #[cfg_attr(docsrs, doc(cfg(feature = "wayland_crate")))]
-use {
-    crate::{ffi, prelude::*},
-    glib::translate::*,
-};
-
-use crate::WaylandMonitor;
+use crate::prelude::*;
+use crate::{WaylandMonitor, ffi};
+use glib::translate::*;
 
 impl WaylandMonitor {
+    #[doc(alias = "gdk_wayland_monitor_get_wl_output")]
+    #[doc(alias = "get_wl_output")]
+    pub fn wl_output_raw(&self) -> Option<NonNull<c_void>> {
+        NonNull::new(unsafe { ffi::gdk_wayland_monitor_get_wl_output(self.to_glib_none().0) })
+    }
+
     #[doc(alias = "gdk_wayland_monitor_get_wl_output")]
     #[doc(alias = "get_wl_output")]
     #[cfg(feature = "wayland_crate")]
     #[cfg_attr(docsrs, doc(cfg(feature = "wayland_crate")))]
     pub fn wl_output(&self) -> Option<WlOutput> {
         let display = self.display().downcast::<crate::WaylandDisplay>().unwrap();
+        let ptr = self.wl_output_raw()?;
         unsafe {
-            let output_ptr = ffi::gdk_wayland_monitor_get_wl_output(self.to_glib_none().0);
-            if output_ptr.is_null() {
-                None
-            } else {
-                let cnx = display.connection();
-                let id = ObjectId::from_ptr(WlOutput::interface(), output_ptr as *mut _).unwrap();
+            let cnx = display.connection();
+            let id = ObjectId::from_ptr(WlOutput::interface(), ptr.as_ptr() as *mut _).unwrap();
 
-                WlOutput::from_id(&cnx, id).ok()
-            }
+            WlOutput::from_id(&cnx, id).ok()
         }
     }
 }
--- gdk4-wayland/src/wayland_surface.rs
+++ gdk4-wayland/src/wayland_surface.rs
@@ -1,13 +1,13 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-#[cfg(feature = "wayland_crate")]
-#[cfg_attr(docsrs, doc(cfg(feature = "wayland_crate")))]
+use std::ffi::c_void;
+use std::ptr::NonNull;
+
 use glib::translate::*;
 #[cfg(feature = "wayland_crate")]
 #[cfg_attr(docsrs, doc(cfg(feature = "wayland_crate")))]
 use wayland_client::{Proxy, backend::ObjectId, protocol::wl_surface::WlSurface};
 
-#[cfg(feature = "wayland_crate")]
 use crate::ffi;
 use crate::{WaylandSurface, prelude::*};
 
@@ -15,6 +15,14 @@ use crate::{WaylandSurface, prelude::*};
 /// Trait containing manually implemented methods of
 /// [`WaylandSurface`](crate::WaylandSurface).
 pub trait WaylandSurfaceExtManual: IsA<WaylandSurface> + 'static {
+    #[doc(alias = "gdk_wayland_surface_get_wl_surface")]
+    #[doc(alias = "get_wl_surface")]
+    fn wl_surface_raw(&self) -> Option<NonNull<c_void>> {
+        NonNull::new(unsafe {
+            ffi::gdk_wayland_surface_get_wl_surface(self.as_ref().to_glib_none().0)
+        })
+    }
+
     #[doc(alias = "gdk_wayland_surface_get_wl_surface")]
     #[doc(alias = "get_wl_surface")]
     #[cfg(feature = "wayland_crate")]
@@ -25,17 +33,12 @@ pub trait WaylandSurfaceExtManual: IsA<WaylandSurface> + 'static {
             .display()
             .downcast::<crate::WaylandDisplay>()
             .unwrap();
+        let ptr = self.wl_surface_raw()?;
         unsafe {
-            let surface_ptr =
-                ffi::gdk_wayland_surface_get_wl_surface(self.as_ref().to_glib_none().0);
-            if surface_ptr.is_null() {
-                None
-            } else {
-                let cnx = display.connection();
-                let id = ObjectId::from_ptr(WlSurface::interface(), surface_ptr as *mut _).unwrap();
+            let cnx = display.connection();
+            let id = ObjectId::from_ptr(WlSurface::interface(), ptr.as_ptr() as *mut _).unwrap();
 
-                WlSurface::from_id(&cnx, id).ok()
-            }
+            WlSurface::from_id(&cnx, id).ok()
         }
     }
 }
